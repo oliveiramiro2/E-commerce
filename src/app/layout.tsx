6325a9d78d645c239b31c4cc932f2e1d83e9f026
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-26 16:59:10 -0300

    added tailwind to project
    creating and config the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
 import React from "react";
-import "./globals.css";
+import "../styles/globals.css";
 
 export const metadata = {
     title: "RM E-commerce",
     description: "A fake e-commerce create with next app",
 };
 
-const RootLayout = ({
-    children,
-}: {
-    children: React.ReactNode;
-}) => {
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="pt-br">
             <body>{children}</body>
         </html>
     );
-}
+};
 
-export default RootLayout
+export default RootLayout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 
-const Home: React.FC = () => {
-    return <main className="" />;
-}
+const Home: React.FC = () => (
+    <main className="h-screen w-screen bg-black flex justify-center items-center">
+        <div className="text-white">teste papai</div>
+    </main>
+);
 
-export default Home
+export default Home;
