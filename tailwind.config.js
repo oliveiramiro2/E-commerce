8ecc10877abacbce2147f4cddba580c84aabfe0e
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-27 20:24:59 -0300

    added pallet colors inside tailwind
    creating and config the project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.tsx", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "pallet-white": "#f7f8f9",
+            "pallet-purple": "#a226d0",
+            "pallet-black": "#160b30",
+            "pallet-blue": "#00baff",
+        }
+    },
   },
   plugins: [],
 }
