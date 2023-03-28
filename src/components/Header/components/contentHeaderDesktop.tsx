c6734e6327c2300b37aa638eab502de06a1df9d1
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-28 03:28:09 -0300

    refatoring the code of header component
    added refator header

---
+++ src/components/Header/components/contentHeaderDesktop.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const ContentHeaderDesktop = () => {
+  return (
+    <div>contentHeaderDesktop</div>
+  )
+}
+
---
+++ src/components/Header/components/index.ts
@@ -0,0 +1,1 @@
+export { ContentHeaderDesktop } from "./contentHeaderDesktop"
