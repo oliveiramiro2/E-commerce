eaa7cab4ed8d04b63f25142ec7979c971d3a95b1
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-28 02:41:08 -0300

    added the firsts adjust on header component
    added responsivity header

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,11 +1,16 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { Tiro_Devanagari_Hindi } from "next/font/google";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const tiro = Tiro_Devanagari_Hindi({ subsets: ["latin"], weight: "400" });
 
 export const Header: React.FC = () => {
+    const [showMenu, setShowMenu] = useState<boolean>(false);
+
     return (
         <header className="bg-pallet-white w-screen h-20 border-b-2 border-pallet-black flex justify-between items-center">
             <Link href="/" className="absolute left-3">
@@ -17,7 +22,7 @@ export const Header: React.FC = () => {
                 />
             </Link>
             <div />
-            <nav className="h-full flex justify-center items-center">
+            <nav className="h-full flex justify-center items-center max-md:hidden">
                 <ul className="flex gap-x-20">
                     <li
                         className={`text-pallet-blue font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
@@ -39,12 +44,25 @@ export const Header: React.FC = () => {
             <div />
             <button
                 type="button"
-                className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange absolute right-5 h-5 p-8 pt-4 pb-4 rounded-md flex justify-center items-center"
+                className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange absolute right-5 h-5 p-8 pt-4 pb-4 rounded-md flex justify-center items-center max-md:hidden"
             >
                 <span className="text-pallet-white font-bold tracking-wider">
                     Login
                 </span>
             </button>
+            {showMenu ? (
+                <AiOutlineClose
+                    className="text-rose-600 cursor-pointer mr-6 hidden max-md:block"
+                    size={25}
+                    onClick={() => setShowMenu(false)}
+                />
+            ) : (
+                <AiOutlineMenu
+                    className="text-pallet-orange cursor-pointer mr-6 hidden max-md:block"
+                    size={25}
+                    onClick={() => setShowMenu(true)}
+                />
+            )}
         </header>
     );
 };
