2931bc6bf5c86e923734dfae16b3840490581c6b
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-28 03:30:47 -0300

    refatoring the code of header component
    added refator header

--- src/components/Header/components/contentHeaderDesktop.tsx
+++ src/components/Header/components/contentHeaderDesktop.tsx
@@ -1,8 +1,69 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { Tiro_Devanagari_Hindi } from "next/font/google";
 
-export const ContentHeaderDesktop = () => {
-  return (
-    <div>contentHeaderDesktop</div>
-  )
-}
+import { IPropsContentHeaderDesktop } from "@/interface";
 
+const tiro = Tiro_Devanagari_Hindi({ subsets: ["latin"], weight: "400" });
+
+export const ContentHeaderDesktop: React.FC<IPropsContentHeaderDesktop> = ({
+    showMenu,
+    setShowMenu,
+}) => {
+    return (
+        <div className="w-screen h-20 bg-pallet-white flex justify-between items-center">
+            <Link href="/" className="absolute left-3">
+                <Image
+                    src="/imgs/ecom.png"
+                    alt="logo"
+                    width={180}
+                    height={20}
+                />
+            </Link>
+            <div />
+            <nav className="h-full flex justify-center items-center max-md:hidden">
+                <ul className="flex gap-x-20">
+                    <li
+                        className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                    >
+                        <Link href="/">Home</Link>
+                    </li>
+                    <li
+                        className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                    >
+                        <Link href="/">Comprar</Link>
+                    </li>
+                    <li
+                        className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                    >
+                        <Link href="/">Carrinho</Link>
+                    </li>
+                </ul>
+            </nav>
+            <div />
+            <button
+                type="button"
+                className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange absolute right-5 h-5 p-8 pt-4 pb-4 rounded-md flex justify-center items-center max-md:hidden"
+            >
+                <span className="text-pallet-white font-bold tracking-wider">
+                    Login
+                </span>
+            </button>
+            {showMenu ? (
+                <AiOutlineClose
+                    className="text-rose-600 cursor-pointer mr-6 hidden max-md:block"
+                    size={25}
+                    onClick={() => setShowMenu(false)}
+                />
+            ) : (
+                <AiOutlineMenu
+                    className="text-pallet-orange cursor-pointer mr-6 hidden max-md:block"
+                    size={25}
+                    onClick={() => setShowMenu(true)}
+                />
+            )}
+        </div>
+    );
+};
--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React, { useState } from "react";
-import Image from "next/image";
 import Link from "next/link";
 import { Tiro_Devanagari_Hindi } from "next/font/google";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { ContentHeaderDesktop } from "./components/contentHeaderDesktop";
 
 const tiro = Tiro_Devanagari_Hindi({ subsets: ["latin"], weight: "400" });
 
@@ -13,58 +12,10 @@ export const Header: React.FC = () => {
 
     return (
         <header className="w-screen min-h-20 border-b-2 border-pallet-black flex flex-col">
-            <div className="w-screen h-20 bg-pallet-white flex justify-between items-center">
-                <Link href="/" className="absolute left-3">
-                    <Image
-                        src="/imgs/ecom.png"
-                        alt="logo"
-                        width={180}
-                        height={20}
-                    />
-                </Link>
-                <div />
-                <nav className="h-full flex justify-center items-center max-md:hidden">
-                    <ul className="flex gap-x-20">
-                        <li
-                            className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
-                        >
-                            <Link href="/">Home</Link>
-                        </li>
-                        <li
-                            className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
-                        >
-                            <Link href="/">Comprar</Link>
-                        </li>
-                        <li
-                            className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
-                        >
-                            <Link href="/">Carrinho</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <div />
-                <button
-                    type="button"
-                    className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange absolute right-5 h-5 p-8 pt-4 pb-4 rounded-md flex justify-center items-center max-md:hidden"
-                >
-                    <span className="text-pallet-white font-bold tracking-wider">
-                        Login
-                    </span>
-                </button>
-                {showMenu ? (
-                    <AiOutlineClose
-                        className="text-rose-600 cursor-pointer mr-6 hidden max-md:block"
-                        size={25}
-                        onClick={() => setShowMenu(false)}
-                    />
-                ) : (
-                    <AiOutlineMenu
-                        className="text-pallet-orange cursor-pointer mr-6 hidden max-md:block"
-                        size={25}
-                        onClick={() => setShowMenu(true)}
-                    />
-                )}
-            </div>
+            <ContentHeaderDesktop
+                showMenu={showMenu}
+                setShowMenu={setShowMenu}
+            />
             {showMenu && (
                 <div className="bg-black pt-2 pb-2 hidden max-md:block">
                     <nav>
@@ -97,7 +48,9 @@ export const Header: React.FC = () => {
                                 className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow shadow-pallet-white h-5 p-20 mt-2 mb-2 pt-4 pb-4 rounded-lg flex justify-center items-center"
                                 type="button"
                             >
-                                <p className="text-pallet-white font-bold tracking-wider">Login</p>
+                                <p className="text-pallet-white font-bold tracking-wider">
+                                    Login
+                                </p>
                             </button>
                         </ul>
                     </nav>
