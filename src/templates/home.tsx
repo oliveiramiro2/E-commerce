55e465f0fcc44df2d78cb99557069befca7f5d53
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-28 01:57:22 -0300

    added a folder template to make code more clean
    added file home template to show page in app folder
    
    added template

---
+++ src/templates/home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Header } from "@/components/Header";
+
+export const HomeTemplate: React.FC = () => (
+    <div className="bg-white h-screen w-screen flex flex-col items-center">
+        <Header />
+        <div className="">teste papai2</div>
+    </div>
+);
