808e66a8021b5bd2047e2b3d34b6f3f57c1091cb
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-30 17:27:10 -0300

    added icons before link words
    added components header

--- src/components/Header/components/contentHeaderDesktop.tsx
+++ src/components/Header/components/contentHeaderDesktop.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { FaHome, FaShoppingBag, FaShoppingCart } from "react-icons/fa";
+import { BiLogIn } from "react-icons/bi";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import { Tiro_Devanagari_Hindi } from "next/font/google";
 
@@ -24,21 +26,28 @@ export const ContentHeaderDesktop: React.FC<IPropsContentHeaderDesktop> = ({
             </Link>
             <div />
             <nav className="h-full flex justify-center items-center max-md:hidden">
-                <ul className="flex gap-x-20">
+                <ul className="flex gap-x-20 max-lg:gap-x-10">
                     <li
                         className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                     >
-                        <Link href="/">Home</Link>
+                        <Link className="flex gap-x-1" href="/">
+                            <FaHome className="relative top-1" /> Home
+                        </Link>
                     </li>
                     <li
                         className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                     >
-                        <Link href="/">Comprar</Link>
+                        <Link className="flex gap-x-1" href="/">
+                            <FaShoppingBag className="relative top-1" /> Comprar
+                        </Link>
                     </li>
                     <li
                         className={`text-pallet-black font-medium text-lg hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                     >
-                        <Link href="/">Carrinho</Link>
+                        <Link className="flex gap-x-1" href="/">
+                            <FaShoppingCart className="relative top-1" />{" "}
+                            Carrinho
+                        </Link>
                     </li>
                 </ul>
             </nav>
@@ -47,7 +56,15 @@ export const ContentHeaderDesktop: React.FC<IPropsContentHeaderDesktop> = ({
                 type="button"
                 className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange absolute right-5 h-5 p-8 pt-4 pb-4 rounded-md flex justify-center items-center max-md:hidden"
             >
-                <Link href="/" className="text-pallet-white font-bold tracking-wider">
+                <Link
+                    href="/"
+                    className="text-pallet-white font-bold tracking-wider flex gap-x-1"
+                >
+                    <BiLogIn
+                        className="relative top-1"
+                        size={18}
+                        color="#f7f8f9"
+                    />
                     Login
                 </Link>
             </button>
--- src/components/Header/components/contentHeaderMobile.tsx
+++ src/components/Header/components/contentHeaderMobile.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Link from "next/link";
+import { FaHome, FaShoppingBag, FaShoppingCart } from "react-icons/fa";
+import { BiLogIn } from "react-icons/bi";
 import { Tiro_Devanagari_Hindi } from "next/font/google";
 
 const tiro = Tiro_Devanagari_Hindi({ subsets: ["latin"], weight: "400" });
@@ -11,25 +13,38 @@ export const ContentHeaderMobile: React.FC = () => {
                 <ul className="flex flex-col gap-y-1 items-center">
                     <li className="border-b w-full border-gray-800 pt-2 pb-5 pl-5">
                         <Link
-                            className={`text-pallet-white font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                            className={`text-pallet-white w-[74px] flex gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                             href="/"
                         >
+                            <FaHome
+                                size={18}
+                                className="relative top-[1px]"
+                                color="#f7f8f9"
+                            />{" "}
                             Home
                         </Link>
                     </li>
                     <li className="border-b w-full border-gray-800 pt-3 pb-5 pl-5">
                         <Link
-                            className={`text-pallet-white font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                            className={`text-pallet-white w-24 flex gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                             href="/"
                         >
+                            <FaShoppingBag
+                                className="relative top-[1px]"
+                                color="#f7f8f9"
+                            />{" "}
                             Comprar
                         </Link>
                     </li>
                     <li className="border-b w-full border-gray-800 pt-3 pb-5 pl-5">
                         <Link
-                            className={`text-pallet-white font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                            className={`text-pallet-white flex w-24 gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                             href="/"
                         >
+                            <FaShoppingCart
+                                className="relative top-[1px]"
+                                color="#f7f8f9"
+                            />{" "}
                             Carrinho
                         </Link>
                     </li>
@@ -37,7 +52,15 @@ export const ContentHeaderMobile: React.FC = () => {
                         className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow shadow-pallet-white h-5 p-20 mt-2 mb-2 pt-4 pb-4 rounded-lg flex justify-center items-center"
                         type="button"
                     >
-                        <Link href="/" className="text-pallet-white font-bold tracking-wider">
+                        <Link
+                            href="/"
+                            className="text-pallet-white flex gap-x-1 font-bold tracking-wider"
+                        >
+                            <BiLogIn
+                                className="relative top-1"
+                                size={18}
+                                color="#f7f8f9"
+                            />
                             Login
                         </Link>
                     </button>
