5d9c5c59f68b3653aba732fe16b74f4c31099f07
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-30 20:35:03 -0300

    added description into bannet section
    added home page

--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -4,8 +4,24 @@ import { DefaultTemplate } from "./default";
 
 export const HomeTemplate: React.FC = () => (
     <DefaultTemplate>
-        <div className="bg-banner bg-cover bg-fixed bg-no-repeat w-screen h-[80vh]">
-            teste papai
-        </div>
+        <section className="bg-banner bg-cover bg-center bg-fixed bg-no-repeat w-screen h-[90vh] flex justify-between">
+            <div />
+            <div className="bg-pallet-white w-[30%] h-[50%] flex flex-col justify-around items-center pl-6 pr-6 rounded-lg border-2 border-pallet-blue mt-10 mr-20">
+                <h1 className="font-bold text-xl text-center">
+                    Bem-vindo ao RM E-commerce
+                </h1>
+                <p className="font-medium tracking-wide">
+                    Este é um E-commerce falso para fins de treinando com a
+                    biblioteca ReactJS, o framework NextJS e uma store fake API
+                    para meu portfólio.
+                </p>
+                <button
+                    type="button"
+                    className="bg-pallet-black p-3 pr-4 pl-4 text-pallet-white rounded-xl font-bold text-sm border-2 border-pallet-orange shadow-lg shadow-pallet-black hover:bg-pallet-orange hover:transition-colors"
+                >
+                    Veja os produtos
+                </button>
+            </div>
+        </section>
     </DefaultTemplate>
 );
