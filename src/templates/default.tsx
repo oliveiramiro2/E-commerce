02582c58e61d907eaee5a9d8647a1264b6023fd7
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-31 02:41:59 -0300

    added a file index into global components to call all components inside it
    adjust default template

--- src/templates/default.tsx
+++ src/templates/default.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
-import { Footer } from "@/components/Footer";
-import { Header } from "@/components/Header";
+import { Footer, Header } from "@/components";
 
 export const DefaultTemplate: React.FC<{ children: React.ReactNode }> = ({
     children,
