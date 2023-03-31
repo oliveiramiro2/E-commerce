9997a51a940f56ce189d50cd83c984968f04c252
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-31 17:00:18 -0300

    added responsivity into all content sing up home section
    responsivity into sign up component

--- src/templates/Home/components/signUp.tsx
+++ src/templates/Home/components/signUp.tsx
@@ -6,20 +6,20 @@ export const SignUpHome: React.FC = () => {
     return (
         <section className="w-screen min-h-[65vh] flex flex-col items-center pt-8">
             <div>
-                <h2 className={`font-black text-3xl ${oswald.className}`}>
+                <h2 className={`font-black text-3xl relative right-5 max-lg:right-0 ${oswald.className}`}>
                     Faça seu cadastro
                 </h2>
             </div>
             <div className="flex mt-8 justify-center gap-x-8 pl-5 pr-5 max-sm:flex-wrap-reverse max-sm:mb-8">
-                <div className="flex flex-col justify-center items-end gap-y-5 max-sm:w-[90%] max-sm:items-center">
+                <div className="flex flex-col w-[30%] max-lg:w-[40%] justify-center items-end gap-y-5 max-sm:w-[90%] max-sm:items-center">
                     <input
                         type="text"
-                        className={`min-w-[20vw] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
+                        className={`min-w-[90%] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
                         placeholder="Digite seu e-mail"
                     />
                     <input
                         type="password"
-                        className={`min-w-[20vw] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
+                        className={`min-w-[90%] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
                         placeholder="Digite sua senha"
                     />
                     <button
@@ -29,9 +29,9 @@ export const SignUpHome: React.FC = () => {
                         Registrar
                     </button>
                 </div>
-                <div className="p-10 max-w-[50%] max-sm:w-[90%] max-md:pl-2 max-md:pr-2">
+                <div className="p-10 w-[40%] max-sm:w-[80%] max-md:pl-2 max-md:pr-2">
                     <p
-                        className={`font-extrabold text-2xl max-lg:text-xl max-md:text-lg tracking-widest ${arnekG.className}`}
+                        className={`font-extrabold text-2xl max-lg:text-lg tracking-widest ${arnekG.className}`}
                     >
                         Faça seu cadastro para usar nosso e-commerce e realizar
                         compras com facilidade. Compre e receba seus produtos no
