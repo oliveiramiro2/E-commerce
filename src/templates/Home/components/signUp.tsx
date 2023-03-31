727d628b79ea1dfeb3a7c25f0c7b36be3c68b5a8
oliveiramiro2 <ramiromares95@gmail.com>
2023-03-31 16:42:06 -0300

    added responsivity into inputs of sing up home section
    responsivity into sign up component

--- src/templates/Home/components/signUp.tsx
+++ src/templates/Home/components/signUp.tsx
@@ -10,28 +10,28 @@ export const SignUpHome: React.FC = () => {
                     Faça seu cadastro
                 </h2>
             </div>
-            <div className="flex mt-8 justify-center gap-x-8">
-                <div className="w-1/3 flex flex-col justify-center items-end gap-y-5">
+            <div className="flex mt-8 justify-center gap-x-8 pl-5 pr-5 max-sm:flex-wrap-reverse max-sm:mb-8">
+                <div className="flex flex-col justify-center items-end gap-y-5 max-sm:w-[90%] max-sm:items-center">
                     <input
                         type="text"
-                        className={`w-[20vw] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
+                        className={`min-w-[20vw] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
                         placeholder="Digite seu e-mail"
                     />
                     <input
                         type="password"
-                        className={`w-[20vw] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
+                        className={`min-w-[20vw] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
                         placeholder="Digite sua senha"
                     />
                     <button
                         type="button"
-                        className={`self-center relative left-[90px] rounded-xl p-2 pl-8 pr-8 bg-pallet-purple text-pallet-white tracking-wide shadow-lg shadow-gray-400 hover:bg-[#bf3eee] hover:transition-colors ${oswald.className}`}
+                        className={`self-center rounded-xl p-2 pl-8 pr-8 bg-pallet-purple text-pallet-white tracking-wide shadow-lg shadow-gray-400 hover:bg-[#bf3eee] hover:transition-colors ${oswald.className}`}
                     >
                         Registrar
                     </button>
                 </div>
-                <div className="w-1/3 p-10">
+                <div className="p-10 max-w-[50%] max-sm:w-[90%] max-md:pl-2 max-md:pr-2">
                     <p
-                        className={`font-extrabold text-2xl tracking-widest ${arnekG.className}`}
+                        className={`font-extrabold text-2xl max-lg:text-xl max-md:text-lg tracking-widest ${arnekG.className}`}
                     >
                         Faça seu cadastro para usar nosso e-commerce e realizar
                         compras com facilidade. Compre e receba seus produtos no
