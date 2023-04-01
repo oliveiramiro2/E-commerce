23a960bc6b01e4a7f266f8c3278fa84c67e23e9e
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-01 15:47:44 -0300

    added a base link that will used in project
    added a API request

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 
-const base_url = ""
+/* const base_url = "https://fakestoreapi.com/" */
+const base_url = "https://api.escuelajs.co/api/v1"
+
 
 export const api = axios.create({
     baseURL: base_url,
