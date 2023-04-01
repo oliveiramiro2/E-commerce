164b5c64900319901e18f1b6912a6b8aed043175
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-01 04:03:23 -0300

    added provider to default page but i think that is not work in another files i'll test it
    added contexts to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+
 import { HomeTemplate } from "@/templates";
+import { ReactQueryProvider } from "@/contexts/reactQuery";
 
 const Home: React.FC = () => (
-    <main className="min-h-screen">
-        <HomeTemplate />
-    </main>
+    <ReactQueryProvider>
+        <main className="min-h-screen">
+            <HomeTemplate />
+        </main>
+    </ReactQueryProvider>
 );
 
 export default Home;
--- src/contexts/reactQuery.tsx
+++ src/contexts/reactQuery.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-export const ReactQueryProvider = () => {
-  return (
-    <div>reactQuery</div>
-  )
-}
+const queryClient = new QueryClient();
 
+export const ReactQueryProvider: React.FC<{ children: React.ReactNode }> = ({
+    children,
+}) => (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
