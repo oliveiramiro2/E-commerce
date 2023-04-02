161e4c8917f78832311bbeb30908e851330807ac
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-02 02:58:28 -0300

    added hook logic and into this component
    added product component

--- src/components/Product/hooks/useShowInfo.ts
+++ src/components/Product/hooks/useShowInfo.ts
@@ -3,6 +3,9 @@ import { useState } from 'react'
 export const useShowInfo = () => {
     const [showInfo, setShowInfo] = useState<boolean>(false)
 
-  return {}
+    return {
+        show: showInfo,
+        handleShow: () => setShowInfo(!showInfo)
+    }
 }
 
--- src/components/Product/product.tsx
+++ src/components/Product/product.tsx
@@ -1,26 +1,46 @@
+"use client";
+
 import React from "react";
 import { SiFireship } from "react-icons/si";
+import clsx from "clsx";
 
 import { IDataApi } from "@/interface";
 import { arnekG } from "@/functions/fonts";
+import { useShowInfo } from "./hooks/useShowInfo";
 
-export const Product: React.FC<{ param: IDataApi }> = ({ param }) => (
-    <div className="bg-pallet-black flex flex-col items-center gap-y-2 p-2 pr-3 pl-3 rounded-xl border-2 border-pallet-orange">
-        <p
-            className={`font-bold first-letter:capitalize text-lg text-pallet-white ${arnekG.className}`}
-        >
-            {param.title}
-        </p>
-        <img
-            src={param.images[0]}
-            alt="imagem produto"
-            className="w-[20vw] max-md:w-[80vw] object-cover"
-        />
-        <div className="flex gap-x-2">
-            <SiFireship color="#f00" />
-            <p className={`font-bold text-sm text-pallet-white ${arnekG.className}`}>
-                R$ {param.price},00
+export const Product: React.FC<{ param: IDataApi }> = ({ param }) => {
+    const { show } = useShowInfo();
+
+    return (
+        <div className="bg-pallet-black flex flex-col items-center gap-y-2 p-2 pr-3 pl-3 rounded-xl border-2 border-pallet-orange">
+            <p
+                className={`font-bold first-letter:capitalize text-lg text-pallet-white ${arnekG.className}`}
+            >
+                {param.title}
             </p>
+            <img
+                src={param.images[0]}
+                alt="imagem produto"
+                className="w-[20vw] max-md:w-[80vw] object-cover"
+            />
+            <p
+                className={clsx(
+                    `font-medium w-[20vw] hidden max-md:w-[80vw] text-center text-sm text-pallet-white first-letter:capitalize ${arnekG.className}`,
+                    {
+                        block: show,
+                    }
+                )}
+            >
+                {param.description}
+            </p>
+            <div className="flex gap-x-2">
+                <SiFireship color="#f00" />
+                <p
+                    className={`font-bold text-sm text-pallet-white ${arnekG.className}`}
+                >
+                    R$ {param.price},00
+                </p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
