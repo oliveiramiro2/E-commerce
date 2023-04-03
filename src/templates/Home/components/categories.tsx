839c277c645318706c6d43efc3ac7a11b1955ef6
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-03 17:45:12 -0300

    added a simple layout of categories
    break to study of trainee week
    
    added categories component

--- src/templates/Home/components/categories.tsx
+++ src/templates/Home/components/categories.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 import { oswald } from "@/functions/fonts";
 import { categories } from "@/services/api";
 import { ICategoryApi } from "@/interface";
+import { ContainCategory } from "./containCategory";
 
 export const Categories: React.FC = () => {
     const { data, isLoading } = useQuery<ICategoryApi[] | undefined>({
@@ -16,7 +17,7 @@ export const Categories: React.FC = () => {
     if (isLoading) return <div>loading...</div>;
 
     return (
-        <section>
+        <section className="w-screen flex flex-col justify-center items-center mb-5">
             <div>
                 <h2
                     className={`font-black text-3xl relative right-5 max-lg:right-0 mb-8 ${oswald.className}`}
@@ -24,10 +25,14 @@ export const Categories: React.FC = () => {
                     Categorias
                 </h2>
             </div>
-            <ul>
+            <div className="flex w-[86%] overflow-x-hidden">
+                <div className="w-full flex gap-x-5">
                 {data !== undefined &&
-                    data.map(value => <li key={value.id}>{value.name}</li>)}
-            </ul>
+                    data.map(value => (
+                        <ContainCategory key={value.id} data={value} />
+                    ))}
+                </div>
+            </div>
         </section>
     );
 };
--- src/templates/Home/components/containCategory.tsx
+++ src/templates/Home/components/containCategory.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export const ContainCategory: React.FC = () => {
-  return (
-    <div>containCategory</div>
-  )
-}
+import { ICategoryApi } from "@/interface";
 
+export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => (
+    <div className="w-24 h-26 flex flex-col justify-between">
+        <p>{data.name}</p>
+        <img src={data.image} alt="imagem categoria" className="w-20 h-20"  />
+    </div>
+);
