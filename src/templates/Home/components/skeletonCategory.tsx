fbefcb6686f01d815f8c7f97ec5b6948fed71e63
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-05 03:48:34 -0300

    added layout of skeleton category
    added skeleton category component

--- src/templates/Home/components/skeletonCategory.tsx
+++ src/templates/Home/components/skeletonCategory.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
-export const SkeletonCategory: React.FC = () => {
-  return (
-    <div>skeletonCategory</div>
-  )
-}
+export const SkeletonCategory: React.FC = () => (
+    <SkeletonTheme>
+        <div className="flex flex-col">
+            <p>
+                <Skeleton
+                    count={1}
+                    className="w-20"
+                    containerClassName="flex flex-col items-center"
+                />
+            </p>
+            <div className="w-20 h-20">
+                <Skeleton
+                    className="w-20 h-20"
+                    containerClassName="w-[100%] h-[100%] flex justify-center"
+                />
+            </div>
+        </div>
+    </SkeletonTheme>
+);
