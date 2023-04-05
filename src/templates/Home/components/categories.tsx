da1efa8a7d739d5d4d88a4b41f37d237a6561058
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-05 03:49:37 -0300

    finished the layout of page
    added home page

--- src/templates/Home/components/categories.tsx
+++ src/templates/Home/components/categories.tsx
@@ -7,6 +7,7 @@ import { oswald } from "@/functions/fonts";
 import { categories } from "@/services/api";
 import { ICategoryApi } from "@/interface";
 import { ContainCategory } from "./containCategory";
+import { SkeletonCategory } from "./skeletonCategory";
 
 export const Categories: React.FC = () => {
     const { data, isLoading } = useQuery<ICategoryApi[] | undefined>({
@@ -14,7 +15,34 @@ export const Categories: React.FC = () => {
         queryKey: ["allCategories"],
     });
 
-    if (isLoading) return <div>loading...</div>;
+    if (isLoading) {
+        return (
+            <div className="w-screen flex flex-col justify-center items-center mb-5">
+                <div>
+                    <h2
+                        className={`font-black text-3xl relative right-5 max-lg:right-0 mb-8 ${oswald.className}`}
+                    >
+                        Categorias
+                    </h2>
+                </div>
+                <div className="flex w-[86%] overflow-x-hidden pb-8">
+                    <div className="w-full flex gap-5 justify-center items-center">
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                        <SkeletonCategory />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 
     return (
         <section className="w-screen flex flex-col justify-center items-center mb-5">
@@ -27,10 +55,10 @@ export const Categories: React.FC = () => {
             </div>
             <div className="flex w-[86%] overflow-x-hidden pb-8">
                 <div className="w-full flex gap-5 justify-center items-center flex-wrap">
-                {data !== undefined &&
-                    data.map(value => (
-                        <ContainCategory key={value.id} data={value} />
-                    ))}
+                    {data !== undefined &&
+                        data.map(value => (
+                            <ContainCategory key={value.id} data={value} />
+                        ))}
                 </div>
             </div>
         </section>
