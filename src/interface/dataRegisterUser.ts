1eed5878bdbe0f8235cdb6919f54a41f8a591c29
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-07 05:17:23 -0300

    added interface to data of a new user
    add types

---
+++ src/interface/dataRegisterUser.ts
@@ -0,0 +1,6 @@
+export interface IDataRegisterUser {
+    name: string;
+    email: string;
+    password: string;
+    avatar: string;
+}
--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,2 +1,3 @@
 export type { IPropsContentHeaderDesktop} from './contentHeaderDesktop'
 export type { IDataApi, ICategoryApi } from './dataApiProduct'
+export type { IDataRegisterUser } from './dataRegisterUser'
