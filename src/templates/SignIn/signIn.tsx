6c360908f0940912d1fb7ef23e584b57bed3f035
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-06 21:19:26 -0300

    added component with inputs
    add sing in page

--- src/templates/SignIn/signIn.tsx
+++ src/templates/SignIn/signIn.tsx
@@ -3,11 +3,16 @@
 import React, { useLayoutEffect } from "react";
 
 import { DefaultTemplate } from "../default";
+import { LoginAndRegister } from "@/components";
 
 export const SignInTemplate: React.FC = () => {
     useLayoutEffect(() => {
         document.title = "RM E-commerce - Cadastro";
     }, []);
 
-    return <DefaultTemplate>signIn</DefaultTemplate>;
+    return (
+        <DefaultTemplate>
+            <LoginAndRegister />
+        </DefaultTemplate>
+    );
 };
