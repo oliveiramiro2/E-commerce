09ffcd947e098d31714018f66df6f8c32126e69d
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-08 03:20:21 -0300

    add component and style to notification works
    add default template

--- src/templates/default.tsx
+++ src/templates/default.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "react-loading-skeleton/dist/skeleton.css";
+import 'react-notifications-component/dist/theme.css'
+import { ReactNotifications } from "react-notifications-component";
 
 import { Footer, Header } from "@/components";
 import RootLayout from "@/app/layout";
@@ -9,6 +11,7 @@ export const DefaultTemplate: React.FC<{ children: React.ReactNode }> = ({
 }) => (
     <RootLayout>
         <main className="bg-white min-h-screen w-screen flex flex-col items-center">
+            <ReactNotifications />
             <Header />
             {children}
             <Footer />
