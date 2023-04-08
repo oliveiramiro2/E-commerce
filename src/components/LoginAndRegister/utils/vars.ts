7cc6e4056a011346ba3a64526d6bc786c7f18b59
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-08 00:47:12 -0300

    added values into vars
    add vars inputs

--- src/components/LoginAndRegister/utils/vars.ts
+++ src/components/LoginAndRegister/utils/vars.ts
@@ -1,13 +1,13 @@
 import { IInputs } from "../interfaces";
 
 export const inputsDataRegister: IInputs[] = [
-    {id: 0, label: '', name: '', placeholder: '', type: ''},
-    {id: 0, label: '', name: '', placeholder: '', type: ''},
-    {id: 0, label: '', name: '', placeholder: '', type: ''},
-    {id: 0, label: '', name: '', placeholder: '', type: ''},
+    {id: 0, label: 'Nome', name: 'name', placeholder: 'Digite seu nome', type: 'text'},
+    {id: 1, label: 'Email', name: 'email', placeholder: 'Digite seu email', type: 'email'},
+    {id: 2, label: 'Senha', name: 'password', placeholder: 'Digite sua senha', type: 'password'},
+    {id: 3, label: 'Confirme sua senha', name: 'confirmPassword', placeholder: 'Repita sua senha', type: 'password'},
 ]
 
 export const inputsDataLogin: IInputs[] = [
-    {id: 0, label: '', name: '', placeholder: '', type: ''},
-    {id: 0, label: '', name: '', placeholder: '', type: ''},
+    {id: 0, label: 'Email', name: 'email', placeholder: 'Digite seu email', type: 'email'},
+    {id: 1, label: 'Senha', name: 'password', placeholder: 'Digite sua senha', type: 'password'},
 ]
