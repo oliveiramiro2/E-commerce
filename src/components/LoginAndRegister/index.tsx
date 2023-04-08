bab0537599da7a6ac3f7f22e4c9dd23682b9d7a7
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-08 03:00:32 -0300

    adjust to make component login and register more clean
    adjust login and register

--- src/components/LoginAndRegister/index.tsx
+++ src/components/LoginAndRegister/index.tsx
@@ -2,15 +2,17 @@
 
 import React from "react";
 
-import { oswald, tiro } from "@/functions/fonts";
-import { useLoginRegister } from "./hooks";
+import { oswald } from "@/functions/fonts";
+import { useLoginOrRegister, useLoginRegister } from "./hooks";
 import { loginInvalid, loginValid } from "./functions";
+import { CheckBoxUser, InputUser } from "./components";
 
 export const LoginAndRegister: React.FC<{ registerComponent: boolean }> = ({
     registerComponent,
 }) => {
     const { errors, handleSubmit, register } =
         useLoginRegister(registerComponent);
+    const { dataInputs } = useLoginOrRegister(registerComponent);
 
     return (
         <section className="w-screen min-h-[72vh] flex justify-center">
@@ -23,114 +25,20 @@ export const LoginAndRegister: React.FC<{ registerComponent: boolean }> = ({
                     </h3>
                 </div>
                 <form className="flex flex-col gap-y-4 w-full items-center">
-                    {registerComponent && (
-                        <div className="flex flex-col w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] items-center">
-                            <label
-                                htmlFor="name"
-                                className={`ml-2 font-semibold self-start ${tiro.className}`}
-                            >
-                                Nome
-                            </label>
-                            <input
-                                type="text"
-                                id="name"
-                                className={`w-full outline-none border-2 border-pallet-purple p-1 pl-2 rounded-lg ${tiro.className}`}
-                                placeholder="Digite seu nome"
-                                {...register("name")}
-                            />
-                            {errors.name?.message && (
-                                <p
-                                    className={`text-red-500 font-semibold self-center relative top-3 ${tiro.className}`}
-                                >
-                                    {errors.name?.message}
-                                </p>
-                            )}
-                        </div>
-                    )}
-                    <div className="flex flex-col w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] items-center">
-                        <label
-                            htmlFor="email"
-                            className={`ml-2 font-semibold self-start ${tiro.className}`}
-                        >
-                            Email
-                        </label>
-                        <input
-                            type="email"
-                            id="email"
-                            className={`w-full outline-none border-2 border-pallet-purple p-1 pl-2 rounded-lg ${tiro.className}`}
-                            placeholder="Digite seu email"
-                            {...register("email")}
+                    {dataInputs.map(item => (
+                        <InputUser
+                            key={item.id}
+                            index={item.id}
+                            data={{
+                                ...item,
+                                register,
+                                errors,
+                                registerForm: registerComponent,
+                            }}
                         />
-                        {errors.email?.message && (
-                            <p
-                                className={`text-red-500 font-semibold self-center relative top-3 ${tiro.className}`}
-                            >
-                                {errors.email?.message}
-                            </p>
-                        )}
-                    </div>
-                    <div className="flex flex-col w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] items-center">
-                        <label
-                            htmlFor="senha"
-                            className={`ml-2 font-semibold self-start ${tiro.className}`}
-                        >
-                            Senha
-                        </label>
-                        <input
-                            type="password"
-                            id="senha"
-                            className={`w-full outline-none border-2 border-pallet-purple p-1 pl-2 rounded-lg ${tiro.className}`}
-                            placeholder="Digite sua senha"
-                            {...register("password")}
-                        />
-                        {errors.password?.message && (
-                            <p
-                                className={`text-red-500 font-semibold self-center relative top-3 ${tiro.className}`}
-                            >
-                                {errors.password?.message}
-                            </p>
-                        )}
-                    </div>
+                    ))}
                     {registerComponent && (
-                        <div className="flex flex-col w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] items-center">
-                            <label
-                                htmlFor="confirmPassword"
-                                className={`ml-2 font-semibold self-start ${tiro.className}`}
-                            >
-                                Confirme sua senha
-                            </label>
-                            <input
-                                type="password"
-                                id="confirmPassword"
-                                className={`w-full outline-none border-2 border-pallet-purple p-1 pl-2 rounded-lg ${tiro.className}`}
-                                placeholder="Repita sua senha"
-                                {...register("confirmPassword")}
-                            />
-                            {errors.confirmPassword?.message && (
-                                <p
-                                    className={`text-red-500 font-semibold self-center relative top-3 ${tiro.className}`}
-                                >
-                                    {errors.confirmPassword?.message}
-                                </p>
-                            )}
-                        </div>
-                    )}
-                    {registerComponent && (
-                        <div className="flex w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] mt-5 items-center pl-3">
-                            <input
-                                type="checkbox"
-                                id="tipo"
-                                className="accent-pallet-purple"
-                                placeholder="Repita sua senha"
-                                {...register("type")}
-                            />
-                            <label
-                                htmlFor="tipo"
-                                className={`ml-2 font-semibold self-start relative top-[2px] ${tiro.className}`}
-                            >
-                                Administrador
-                            </label>
-                        </div>
+                        <CheckBoxUser register={register} />
                     )}
                     <button
                         type="button"
