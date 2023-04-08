45e38aca90df5925b7e7c1c737689830704e2ec1
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-08 00:53:12 -0300

    added logic to use data inputs
    add hook to login and register

--- src/components/LoginAndRegister/hooks/useLoginOrRegister.tsx
+++ src/components/LoginAndRegister/hooks/useLoginOrRegister.tsx
@@ -1,3 +1,11 @@
-export const useLoginOrRegister = () => {
-    
-}
+import { useState } from "react";
+import { IInputs } from "../interfaces";
+import { inputsDataLogin, inputsDataRegister } from "../utils";
+
+export const useLoginOrRegister = (register: boolean) => {
+    const [dataInputs] = useState<IInputs[]>(
+        register ? inputsDataRegister : inputsDataLogin
+    );
+
+    return {dataInputs}
+};
