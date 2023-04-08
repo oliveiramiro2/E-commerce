0ecf50e06afc87a6987049706ef9ca8d399f488e
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-08 00:40:32 -0300

    added a component to render inputs
    add vars inputs

---
+++ src/components/LoginAndRegister/utils/vars.ts
@@ -0,0 +1,13 @@
+import { IInputs } from "../interfaces";
+
+export const inputsDataRegister: IInputs[] = [
+    {id: 0, label: '', name: '', placeholder: '', type: ''},
+    {id: 0, label: '', name: '', placeholder: '', type: ''},
+    {id: 0, label: '', name: '', placeholder: '', type: ''},
+    {id: 0, label: '', name: '', placeholder: '', type: ''},
+]
+
+export const inputsDataLogin: IInputs[] = [
+    {id: 0, label: '', name: '', placeholder: '', type: ''},
+    {id: 0, label: '', name: '', placeholder: '', type: ''},
+]
