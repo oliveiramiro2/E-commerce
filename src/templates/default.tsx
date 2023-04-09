e80500d45d72afc424dee475c983372038e31634
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-08 21:57:35 -0300

    added a save pointer it's work xD

--- src/templates/default.tsx
+++ src/templates/default.tsx
@@ -5,17 +5,14 @@ import "react-notifications-component/dist/theme.css";
 import "animate.css";
 
 import { Footer, Header } from "@/components";
-import RootLayout from "@/app/layout";
 
 export const DefaultTemplate: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => (
-    <RootLayout>
-        <div className="bg-white min-h-screen w-screen flex flex-col items-center">
-            <ReactNotifications />
-            <Header />
-            {children}
-            <Footer />
-        </div>
-    </RootLayout>
+    <div className="bg-white min-h-screen w-screen flex flex-col items-center">
+        <ReactNotifications />
+        <Header />
+        {children}
+        <Footer />
+    </div>
 );
