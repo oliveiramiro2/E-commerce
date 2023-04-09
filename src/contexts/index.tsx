a40fc25e47a85271c2bf13687064e283f400b00c
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-09 08:11:49 -0300

    called context over app
    add context

--- src/contexts/index.tsx
+++ src/contexts/index.tsx
@@ -4,11 +4,14 @@ import React from "react";
 
 import RegisterHomeProvider from "./registerUserHome";
 import { ReactQueryProvider } from "./reactQuery";
+import UserDataLogin from "./userDataLogin";
 
 const ECommProvider = ({ children }: { children: React.ReactNode }) => (
-    <RegisterHomeProvider>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
-    </RegisterHomeProvider>
+    <UserDataLogin>
+        <RegisterHomeProvider>
+            <ReactQueryProvider>{children}</ReactQueryProvider>
+        </RegisterHomeProvider>
+    </UserDataLogin>
 );
 
 export default ECommProvider;
