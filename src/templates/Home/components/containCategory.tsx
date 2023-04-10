c17597bbc7617f9b92463f607820024547465aa9
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-10 06:49:28 -0300

    adjusted links in contain category to move user to make login first
    when he want to buy something
    
    adjust contain category

--- src/templates/Home/components/containCategory.tsx
+++ src/templates/Home/components/containCategory.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 
 import { ICategoryApi } from "@/interface";
 import { oswald } from "@/functions/fonts";
+import { UserDataContext } from "@/contexts/userDataLogin";
 
-export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => (
-    <Link href="/" className="cursor-pointer">
+export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => {
+    const { logined } = useContext(UserDataContext);
+
+    return (
+    <Link href={logined ? "/" : "/entrar"} className="cursor-pointer">
         <div className="w-24 h-26 flex flex-col justify-between items-center rounded-lg pb-2 shadow-xl shadow-gray-500 bg-gradient-to-r to-pallet-orange via-pallet-white from-pallet-purple">
             <p
                 className={`font-medium text-pallet-black first-letter:capitalize ${oswald.className}`}
@@ -19,4 +23,4 @@ export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => (
             />
         </div>
     </Link>
-);
+)};
