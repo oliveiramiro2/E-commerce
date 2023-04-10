67ab3df66d38b5b60c65b9a94f0cc8b5a6a144d2
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-10 05:32:43 -0300

    added a link to navigate amoung register and login page
    add login and register

--- src/components/LoginAndRegister/index.tsx
+++ src/components/LoginAndRegister/index.tsx
@@ -2,8 +2,9 @@
 
 import React, { useContext } from "react";
 import LoadingIcons from "react-loading-icons";
+import Link from "next/link";
 
-import { oswald } from "@/functions/fonts";
+import { arnekG, oswald } from "@/functions/fonts";
 import { useLoginOrRegister, useLoginRegister, useShowLoading } from "./hooks";
 import { loginInvalid, loginValid } from "./functions";
 import { CheckBoxUser, InputUser } from "./components";
@@ -21,7 +22,7 @@ export const LoginAndRegister: React.FC<{ registerComponent: boolean }> = ({
     return (
         <section className="w-screen min-h-[72vh] flex justify-center">
             <div className="w-[80%] max-md:w-[90%] flex flex-col items-center mb-8">
-                <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex justify-center mb-3 mt-3">
+                <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex justify-center mb-3 mt-6">
                     <h3
                         className={`font-black text-3xl self-center max-lg:right-0 ${oswald.className}`}
                     >
@@ -41,6 +42,14 @@ export const LoginAndRegister: React.FC<{ registerComponent: boolean }> = ({
                             }}
                         />
                     ))}
+                    <Link
+                        href={registerComponent ? "/entrar" : "/cadastro"}
+                        className={`text-pallet-blue font-bold tracking-wide hover:border-b hover:border-pallet-blue ${arnekG.className}`}
+                    >
+                        {registerComponent
+                            ? "Já possui uma conta? Faça seu login"
+                            : "Não possui uma conta? Faça seu registro"}
+                    </Link>
                     {registerComponent && <CheckBoxUser register={register} />}
                     <button
                         type="button"
