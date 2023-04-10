3d143682f0d5ba4d2e60f11248f372842da55bef
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-10 06:13:05 -0300

    adjusted in header mobile
    adjust header

--- src/components/Header/components/contentHeaderMobile.tsx
+++ src/components/Header/components/contentHeaderMobile.tsx
@@ -1,68 +1,63 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 import { FaHome, FaShoppingBag, FaShoppingCart } from "react-icons/fa";
 import { BiLogIn } from "react-icons/bi";
 
 import { tiro } from "@/functions/fonts";
+import { UserDataContext } from "@/contexts/userDataLogin";
 
 export const ContentHeaderMobile: React.FC = () => {
+    const { logined } = useContext(UserDataContext);
+
     return (
         <div className="bg-black pt-2 pb-2 hidden max-md:block">
             <nav>
                 <ul className="flex flex-col gap-y-1 items-center">
                     <li className="border-b w-full border-gray-800 pt-2 pb-5 pl-5">
                         <Link
-                            className={`text-pallet-white w-[74px] flex gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                            className={`text-pallet-white w-[74px] flex items-center gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                             href="/"
                         >
-                            <FaHome
-                                size={18}
-                                className="relative top-[1px]"
-                                color="#f7f8f9"
-                            />{" "}
+                            <FaHome size={18} className="relative bottom-[2px]" />{" "}
                             Home
                         </Link>
                     </li>
                     <li className="border-b w-full border-gray-800 pt-3 pb-5 pl-5">
                         <Link
-                            className={`text-pallet-white w-24 flex gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                            className={`text-pallet-white w-24 flex items-center gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                             href="/"
                         >
-                            <FaShoppingBag
-                                className="relative top-[1px]"
-                                color="#f7f8f9"
-                            />{" "}
+                            <FaShoppingBag className="relative bottom-[2px]" />{" "}
                             Comprar
                         </Link>
                     </li>
                     <li className="border-b w-full border-gray-800 pt-3 pb-5 pl-5">
                         <Link
-                            className={`text-pallet-white flex w-24 gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                            className={`text-pallet-white flex items-center w-24 gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
                             href="/"
                         >
-                            <FaShoppingCart
-                                className="relative top-[1px]"
-                                color="#f7f8f9"
-                            />{" "}
+                            <FaShoppingCart className="relative bottom-[2px]" />{" "}
                             Carrinho
                         </Link>
                     </li>
-                    <button
-                        className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow shadow-pallet-white h-5 p-20 mt-2 mb-2 pt-4 pb-4 rounded-lg flex justify-center items-center"
-                        type="button"
-                    >
-                        <Link
-                            href="/entrar"
-                            className="text-pallet-white flex gap-x-1 font-bold tracking-wider"
+                    {!logined && (
+                        <button
+                            className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow shadow-pallet-white h-5 p-20 mt-2 mb-2 pt-4 pb-4 rounded-lg flex justify-center items-center"
+                            type="button"
                         >
-                            <BiLogIn
-                                className="relative top-1"
-                                size={18}
-                                color="#f7f8f9"
-                            />
-                            Login
-                        </Link>
-                    </button>
+                            <Link
+                                href="/entrar"
+                                className="text-pallet-white flex gap-x-1 font-bold tracking-wider"
+                            >
+                                <BiLogIn
+                                    className="relative top-1"
+                                    size={18}
+                                    color="#f7f8f9"
+                                />
+                                Login
+                            </Link>
+                        </button>
+                    )}
                 </ul>
             </nav>
         </div>
