32fb534224a73ff35466513dd33a0007182c36cf
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-11 08:47:17 -0300

    added a check and advice to check local storage if have tokens saved
    add into context

--- src/contexts/userDataLogin.tsx
+++ src/contexts/userDataLogin.tsx
@@ -1,7 +1,14 @@
 "use client";
 
 import React, { createContext, useMemo, useState } from "react";
-import { IContextLoginData, ILoginData } from "@/interface";
+import {
+    IContextLoginData,
+    IDataUser,
+    ILoginData,
+    ILoginTokens,
+} from "@/interface";
+import { getTokens, removeTokens } from "@/services/localStorage";
+import { getLoginData } from "@/services/api";
 
 export const UserDataContext = createContext<IContextLoginData>(
     {} as IContextLoginData
@@ -9,23 +16,38 @@ export const UserDataContext = createContext<IContextLoginData>(
 
 const UserDataLogin = ({ children }: { children: React.ReactNode }) => {
     const [allUserData, setAllUserData] = useState<ILoginData>({
-        access_token: '',
-        refresh_token: '',
-        avatar: '',
-        email: '',
+        access_token: "",
+        refresh_token: "",
+        avatar: "",
+        email: "",
         id: 0,
-        name: '',
-        password: '',
-        role: 'customer',
-    })
-    const [logined, setLogined] = useState<boolean>(false)
+        name: "",
+        password: "",
+        role: "customer",
+    });
+    const [logined, setLogined] = useState<boolean>(false);
 
+    if (typeof window !== "undefined") {
+        /* Perform localStorage action */
+        const tokens: ILoginTokens | false = getTokens();
+        if (tokens !== false) {
+            getLoginData(tokens.access_token)
+                .then((data: IDataUser) => {
+                    setAllUserData({ ...tokens, ...data });
+                    setLogined(true);
+                })
+                .catch(() => {
+                    console.log("foi excluido!");
+                    removeTokens();
+                });
+        }
+    }
     const valueContext = useMemo(
         () => ({
             allUserData,
             setAllUserData,
             logined,
-            setLogined
+            setLogined,
         }),
         [allUserData, logined]
     );
@@ -35,6 +57,6 @@ const UserDataLogin = ({ children }: { children: React.ReactNode }) => {
             {children}
         </UserDataContext.Provider>
     );
-}
+};
 
-export default UserDataLogin
+export default UserDataLogin;
