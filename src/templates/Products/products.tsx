3f8e7a28a678971b5279533e1403b31b9fa810b2
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-11 09:28:29 -0300

    added a folder and file for template products
    add template page

---
+++ src/templates/Products/products.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const Products: React.FC = () => {
+  return (
+    <div>products</div>
+  )
+}
+
--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,3 +1,4 @@
 export { HomeTemplate } from './Home/home';
-export { SignInTemplate } from './SignIn/signIn'
-export { SignUpTemplate } from './SignUp/signUp'
+export { SignInTemplate } from './SignIn/signIn';
+export { SignUpTemplate } from './SignUp/signUp';
+export { Products } from './Products/products';
