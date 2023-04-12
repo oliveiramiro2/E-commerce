4cd19df9378987dcbc041045448741d799560577
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-12 19:53:48 -0300

    added logic to show initial price
    add hooks

--- src/templates/Cart/hooks/usePriceItems.tsx
+++ src/templates/Cart/hooks/usePriceItems.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useState } from "react";
 
 export const usePriceItems = () => {
@@ -8,6 +10,10 @@ export const usePriceItems = () => {
         handlePriceItems: (oldItems: number, items: number, price: number) => {
             const newTotal = priceItems - oldItems * price + items * price;
             setPriceItems(newTotal);
+        },
+        handleFirstTime: (price: number) => {
+            const oldPrice = priceItems
+            setPriceItems(oldPrice + price)
         },
     };
 };
