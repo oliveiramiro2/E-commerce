dac3406310fc58052ad5600879e518106cc7eb6b
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-12 02:03:27 -0300

    added style into title and padding contain cart
    add cart

--- src/templates/Cart/cart.tsx
+++ src/templates/Cart/cart.tsx
@@ -16,20 +16,20 @@ export const CartTemplate: React.FC = () => {
 
     return (
         <DefaultTemplate>
-            <section className="w-screen min-h-[88vh]">
-                <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex justify-center mb-3 mt-6">
+            <section className="w-screen min-h-[72vh] flex flex-col items-center">
+                <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex flex-col justify-center mb-3 mt-6">
                     <h3
-                        className={`font-black text-3xl self-center max-lg:right-0 ${oswald.className}`}
+                        className={`font-black relative right-6 text-3xl self-center max-lg:right-0 ${oswald.className}`}
                     >
                         Carrinho
                     </h3>
                     <span
-                        className={`text-pallet-black font-bold tracking-wide hover:border-b hover:border-pallet-blue ${arnekG.className}`}
+                        className={`text-pallet-black relative left-8 self-center font-bold tracking-wide hover:border-b hover:border-pallet-blue ${arnekG.className}`}
                     >
                         Compre agora seus produtos agora!
                     </span>
                 </div>
-                <div className="w-full">
+                <div className="w-full pl-5 pr-5">
                     {cartData.map((item) => (
                         <ContainCart key={item.id} data={item} />
                     ))}
