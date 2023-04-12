63e05ec542c009e53a106dea418507b89f9a96da
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-12 03:15:52 -0300

    added img and style
    add contain cart

--- src/templates/Cart/components/containCart.tsx
+++ src/templates/Cart/components/containCart.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import { IDataApi } from '@/interface'
 
-export const ContainCart: React.FC<{data: IDataApi}> = () => {
-  return (
-    <div>containCart</div>
+export const ContainCart: React.FC<{data: IDataApi}> = ({data}) => (
+    <div className='bg-white border flex items-center border-gray-200 p-6 pl-1 rounded-lg'>
+        <img alt='produto' src={data.images[0]} className='w-32 h-24 rounded-lg shadow-lg shadow-black' />
+    </div>
   )
-}
