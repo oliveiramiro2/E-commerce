d921c66df8ac9f012cec8a954e90199de88897c1
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-12 01:06:07 -0300

    moved hook use redirect to src folder to use in another component
    adjust hooks

--- src/components/LoginAndRegister/hooks/index.ts
+++ src/components/LoginAndRegister/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useLoginRegister } from './useLoginRegister';
 export { useLoginOrRegister } from './useLoginOrRegister';
 export { useShowLoading } from './useShowLoading';
-export { useRedirect } from './useRedirect';
