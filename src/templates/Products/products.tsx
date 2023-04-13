ed035611a742a64a0bb2fecfc03833847a4ce50f
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-13 02:27:27 -0300

    added a loading skeleton
    add product

--- src/templates/Products/products.tsx
+++ src/templates/Products/products.tsx
@@ -7,7 +7,7 @@ import { DefaultTemplate } from "../default";
 import { oswald } from "@/functions/fonts";
 import { allProducts } from "@/services/api";
 import { IDataApi } from "@/interface";
-import { Product } from "@/components";
+import { Product, SkeletonProducts } from "@/components";
 
 export const ProductsTemplate: React.FC = () => {
     const { data, isLoading } = useQuery<IDataApi[] | undefined>({
@@ -20,7 +20,24 @@ export const ProductsTemplate: React.FC = () => {
     }, []);
 
     if (isLoading) {
-        return <div>loading...</div>
+        return (
+            <DefaultTemplate>
+                <section className="w-screen min-h-[72vh] bg-gray-100 flex flex-col items-center">
+                    <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex flex-col justify-center mb-3 mt-6">
+                        <h3
+                            className={`font-black text-3xl self-center max-lg:right-0 ${oswald.className}`}
+                        >
+                            Produtos
+                        </h3>
+                    </div>
+                    <div className="w-full flex flex-wrap justify-between pl-6 pr-6">
+                        <SkeletonProducts />
+                        <SkeletonProducts />
+                        <SkeletonProducts />
+                    </div>
+                </section>
+            </DefaultTemplate>
+        );
     }
 
     return (
