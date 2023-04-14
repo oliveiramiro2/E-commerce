0a26f2775f10906ee6d17f955bbbb707317de78b
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-14 04:01:54 -0300

    added a file to filters hook add only options for while
    add hooks

--- src/templates/Products/hooks/index.ts
+++ src/templates/Products/hooks/index.ts
@@ -1,1 +1,2 @@
 export { usePagination } from './usePagination';
+export { useFilter } from './useFilter';
---
+++ src/templates/Products/hooks/useFilter.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+import { ICategoriesOptions } from "../interfaces"
+
+export const useFilter = () => {
+    const [options, setOptions] = useState<ICategoriesOptions[]>([])
+
+    return {
+        options,
+        setOptions,
+    }
+}
