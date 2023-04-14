9ae772cdd1ac225fb49c5b298ddc80399f86e55f
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-14 04:27:29 -0300

    added logic to keep name
    add hook and filters

--- src/templates/Products/hooks/useFilter.tsx
+++ src/templates/Products/hooks/useFilter.tsx
@@ -6,9 +6,12 @@ import { ICategoriesOptions } from "../interfaces"
 
 export const useFilter = () => {
     const [options, setOptions] = useState<ICategoriesOptions[]>([])
+    const [name, setName] = useState<string>('')
 
     return {
         options,
         setOptions,
+        name,
+        setName,
     }
 }
