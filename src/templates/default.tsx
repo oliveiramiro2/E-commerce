f44b7303f64723911689c211b319b8fab845a1bd
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-15 00:05:07 -0300

    added a logic to redirect if router is public or private
    add template default

--- src/templates/default.tsx
+++ src/templates/default.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { ReactNotifications } from "react-notifications-component";
+import { usePathname } from "next/navigation";
 
 import { Footer, Header } from "@/components";
+import { checkRoute } from "@/functions/checkRoute";
 
 export const DefaultTemplate: React.FC<{ children: React.ReactNode }> = ({
     children,
-}) => (
-    <div className="bg-white min-h-screen w-screen flex flex-col items-center">
-        <ReactNotifications />
-        <Header />
-        {children}
-        <Footer />
-    </div>
-);
+}) => {
+    const path = usePathname();
+    const isPublicRoute = checkRoute(path);
+
+    return (
+        <div className="bg-white min-h-screen w-screen flex flex-col items-center">
+            {isPublicRoute && (
+                <>
+                    <ReactNotifications />
+                    <Header />
+                    {children}
+                    <Footer />
+                </>
+            )}
+            {!isPublicRoute && (
+                <>
+                    <ReactNotifications />
+                    <Header />
+                    {children}
+                    <Footer />
+                </>
+            )}
+        </div>
+    );
+};
