a592d199cf4a8117b57b8232560c1cab98a4eb1f
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-14 23:52:07 -0300

    added a const to check if router is public or private
    add constants

---
+++ src/constants/appRoutes.ts
@@ -0,0 +1,11 @@
+export const APP_ROUTES = {
+    private: {
+        products: '/produtos',
+        cart: '/carrinho',
+    },
+    public: {
+        home: '/',
+        signIn: '/entrar',
+        signUp: '/cadastrar'
+    },
+}
