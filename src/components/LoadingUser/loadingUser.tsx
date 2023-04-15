4f1c8c34968c800ffe5618fc42da757a0fd9b9a4
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-15 01:12:20 -0300

    add component loading
    add component

---
+++ src/components/LoadingUser/loadingUser.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import LoadingIcons from "react-loading-icons";
+
+import { arnekG } from "@/functions/fonts";
+
+export const LoadingUser: React.FC = () => (
+    <div className="w-screen h-screen bg-white flex justify-center items-center">
+        <div>
+            <p
+                className={`text-pallet-purple font-black tracking-wider ${arnekG.className}`}
+            >
+                Carregando dados....
+            </p>
+                <LoadingIcons.Bars
+                    color="#a226d0"
+                    alignmentBaseline="central"
+                    height={50}
+                    fill="#a226d0"
+                />
+                <LoadingIcons.SpinningCircles
+                color="#a226d0"
+                alignmentBaseline="central"
+                height={50}
+                fill="#a226d0"
+            />
+        </div>
+    </div>
+);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
-export { PrivateRoutes } from "./PrivateRoutes/privateRoutes";
-export { Header } from "./Header"
-export { Footer } from "./Footer"
-export { SkeletonProducts } from './SkeletonProducts'
-export { Product } from './Product'
-export { LoginAndRegister } from './LoginAndRegister'
+export { PrivateRoutes } from './PrivateRoutes/privateRoutes';
+export { Header } from './Header';
+export { Footer } from './Footer';
+export { SkeletonProducts } from './SkeletonProducts';
+export { Product } from './Product';
+export { LoginAndRegister } from './LoginAndRegister';
+export { LoadingUser } from './LoadingUser/loadingUser';
