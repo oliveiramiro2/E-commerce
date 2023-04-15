c521f2d26a29d883ce2c7e10551465827689a599
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-15 04:42:31 -0300

    adjusted to redirect to sing in when is not loggined
    adjust components

--- src/components/PrivateRoutes/privateRoutes.tsx
+++ src/components/PrivateRoutes/privateRoutes.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import React, { useContext, useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 import { APP_ROUTES } from "@/constants/appRoutes";
 import { UserDataContext } from "@/contexts/userDataLogin";
+import { useRedirect } from "@/hooks";
 
 export const PrivateRoutes = ({ children }: { children: React.ReactNode }) => {
     const { logined } = useContext(UserDataContext)
-    const { push } = useRouter();
+    const { push } = useRedirect();
 
     useEffect(() => {
-        if (logined) {
+        if (!logined) {
             push(APP_ROUTES.public.signIn);
         }
     }, [logined, push]);
