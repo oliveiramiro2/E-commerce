bac67fa2911ff72ce65dfe230864f99d3d2cdf13
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-15 01:13:26 -0300

    add logic to show loading page while check if user is loading
    and then check if route that him try access is valid in agree that
    
    add template

--- src/templates/default.tsx
+++ src/templates/default.tsx
@@ -1,29 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ReactNotifications } from "react-notifications-component";
 import { usePathname } from "next/navigation";
 
-import { Footer, Header } from "@/components";
+import { Footer, Header, LoadingUser } from "@/components";
 import { checkRoute } from "@/functions/checkRoute";
+import { UserDataContext } from "@/contexts/userDataLogin";
 
 export const DefaultTemplate: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => {
     const path = usePathname();
     const isPublicRoute = checkRoute(path);
+    const { checkedLogined } = useContext(UserDataContext);
+
+    if (!checkedLogined) {
+        return <LoadingUser />;
+    }
 
     return (
         <div className="bg-white min-h-screen w-screen flex flex-col items-center">
-            {isPublicRoute && (
+            <ReactNotifications />
+            {checkedLogined && isPublicRoute && (
                 <>
-                    <ReactNotifications />
                     <Header />
                     {children}
                     <Footer />
                 </>
             )}
-            {!isPublicRoute && (
+            {checkedLogined && !isPublicRoute && (
                 <>
-                    <ReactNotifications />
                     <Header />
                     {children}
                     <Footer />
