b03d84972602f4361538eaf993a5beefce532f34
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-15 06:57:03 -0300

    added logic in component category to search by category
    add components

--- src/templates/Home/components/containCategory.tsx
+++ src/templates/Home/components/containCategory.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import Link from "next/link";
 
 import { ICategoryApi } from "@/interface";
 import { oswald } from "@/functions/fonts";
+import { useRedirect } from "@/hooks";
 
 export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => {
+    const { push } = useRedirect();
+
     return (
-        <Link href="/produtos" className="cursor-pointer">
+        <button
+            type="button"
+            onClick={() => push(`produtos?categoria=${data.id}`)}
+            className="cursor-pointer"
+        >
             <div className="min-w-[100px] max-w-[120px] pl-2 pr-2 flex flex-col justify-between items-center rounded-lg pb-2 shadow-xl shadow-gray-500 bg-gradient-to-r to-pallet-orange via-pallet-white from-pallet-purple">
                 <p
                     className={`font-medium text-center text-pallet-black first-letter:capitalize ${oswald.className}`}
@@ -19,6 +25,6 @@ export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => {
                     className="w-auto h-20"
                 />
             </div>
-        </Link>
+        </button>
     );
 };
