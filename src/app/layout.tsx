c15964d5500b6afcff6c81971e174ec636e22bc4
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-15 00:05:45 -0300

    adjusted in code
    adjust layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,25 @@
 import React from "react";
+import "animate.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import "react-notifications-component/dist/theme.css";
 
 import "../styles/globals.css";
 import ECommProvider from "@/contexts";
-import "react-loading-skeleton/dist/skeleton.css";
-import "react-notifications-component/dist/theme.css";
-import "animate.css";
+import { PrivateRoutes } from "@/components";
 
 export const metadata = {
     title: "RM E-commerce",
     description: "A fake e-commerce create with next app",
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <html lang="pt-br">
-            <body>
-                <ECommProvider>
-                    {children}
-                </ECommProvider>
-            </body>
-        </html>
-    );
-};
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+    <html lang="pt-br">
+        <body>
+            <ECommProvider>
+                <PrivateRoutes>{children}</PrivateRoutes>
+            </ECommProvider>
+        </body>
+    </html>
+);
 
 export default RootLayout;
