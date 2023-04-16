dde9cdea43f47c40df6bc1fa2681ddb17a42188d
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-16 02:50:39 -0300

    added logic to keep param when user is not loggined
    add component

--- src/templates/Home/components/containCategory.tsx
+++ src/templates/Home/components/containCategory.tsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { ICategoryApi } from "@/interface";
 import { oswald } from "@/functions/fonts";
 import { useRedirect } from "@/hooks";
+import { UserDataContext } from "@/contexts/userDataLogin";
 
 export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => {
     const { push } = useRedirect();
+    const { logined, setRedirectOnLogin } = useContext(UserDataContext)
 
     return (
         <button
             type="button"
-            onClick={() => push(`produtos?categoria=${data.id}`)}
+            onClick={() => {
+                if (!logined) setRedirectOnLogin(`redirecionar=produtos?categoria=${data.id}`)
+                push(`produtos?categoria=${data.id}`)
+            }}
             className="cursor-pointer"
         >
             <div className="min-w-[100px] max-w-[120px] pl-2 pr-2 flex flex-col justify-between items-center rounded-lg pb-2 shadow-xl shadow-gray-500 bg-gradient-to-r to-pallet-orange via-pallet-white from-pallet-purple">
