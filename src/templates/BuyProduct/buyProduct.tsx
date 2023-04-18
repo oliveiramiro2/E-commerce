69cd3489805efd36aabed5c3d06f5bdbdbbd65b3
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-18 02:16:20 -0300

    added a title of page left the name of product
    add template

--- src/templates/BuyProduct/buyProduct.tsx
+++ src/templates/BuyProduct/buyProduct.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 
 import { DefaultTemplate } from "../default";
+import { oswald } from "@/functions/fonts";
 
 export const BuyProductTemplate: React.FC = () => {
     useEffect(() => {
@@ -9,7 +10,16 @@ export const BuyProductTemplate: React.FC = () => {
 
     return (
         <DefaultTemplate>
-            <div>buyProduct</div>
+            <section className="w-screen min-h-[72vh] bg-gray-100 flex flex-col items-center">
+                <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex flex-col justify-center mb-3 mt-6">
+                    <h3
+                        className={`font-black text-3xl self-center max-lg:right-0 ${oswald.className}`}
+                    >
+                        Comprar 
+                    </h3>
+                </div>
+                buyProduct
+            </section>
         </DefaultTemplate>
     );
 };
