1d603b92f2248a11345502259b9cc4cb02253fad
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-22 05:49:20 -0300

    added interface to get address
    add interface

---
+++ src/components/FinishedSeller/interface/address.ts
@@ -0,0 +1,9 @@
+export interface IAddressSchema {
+    CEP: string;
+    street: string;
+    number: number;
+    neiborhood: string;
+    complement: string;
+    city: string;
+    state: string;
+}
