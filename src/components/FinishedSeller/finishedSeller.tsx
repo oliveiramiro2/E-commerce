f9112d9084a5b7a891c6065ca2de246f894e2ca4
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-22 05:01:59 -0300

    added starting build layout of form
    add component

--- src/components/FinishedSeller/finishedSeller.tsx
+++ src/components/FinishedSeller/finishedSeller.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
-export const FinishedSeller: React.FC<{close: Function}> = () => {
-  return (
-    <div>finishedSeller</div>
-  )
-}
+import { tiro } from "@/functions/fonts";
+
+export const FinishedSeller: React.FC<{ close: Function }> = () => {
+    return (
+        <div>
+            <h5>Adicione seu endereço para entrega!</h5>
+            <form>
+                <div>
+                    <label htmlFor="CEP">CEP</label>
+                    <input
+                        name="CEP"
+                        placeholder="Adicione seu CEP"
+                        type="text"
+                        className={`outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
+                    />
+                </div>
+            </form>
+        </div>
+    );
+};
