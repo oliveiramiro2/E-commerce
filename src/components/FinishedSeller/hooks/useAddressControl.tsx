ef71d88559e243b59f2a3bad9ea52e26009a300c
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-22 06:52:53 -0300

    added mask to zip code
    add hook

--- src/components/FinishedSeller/hooks/useAddressControl.tsx
+++ src/components/FinishedSeller/hooks/useAddressControl.tsx
@@ -2,9 +2,11 @@
 
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
 
 import { schemaRegister } from "@/components/LoginAndRegister/utils";
 import { formType } from "../interface";
+import { zipCodeMask } from "@/functions/zipCodeMask";
 
 export const useAddressControl = () => {
     const schema = schemaRegister;
@@ -12,6 +14,8 @@ export const useAddressControl = () => {
     const {
         handleSubmit,
         register,
+        setValue,
+        watch,
         formState: { errors },
     } = useForm<formType>({
         criteriaMode: "all",
@@ -28,6 +32,10 @@ export const useAddressControl = () => {
             street: "",
         },
     });
+
+    useEffect(() => {
+        setValue('CEP', zipCodeMask(watch('CEP')))
+    }, [watch('CEP')])
 
     return {
         handleSubmit,
