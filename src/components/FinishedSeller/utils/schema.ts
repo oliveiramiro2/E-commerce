291dab8dc3a9d86b6cd5d64df9ac60a57e9937ea
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-22 06:14:56 -0300

    adjusted schema
    adjust utils

--- src/components/FinishedSeller/utils/schema.ts
+++ src/components/FinishedSeller/utils/schema.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const schemaAddress = z.object({
-    CEP: z.string().regex(/^\d{5}$/, "CEP inválido"),
+    CEP: z.string().min(9, "Digite um Cep válido!"),
     street: z.string().min(1, "Digite o nome da sua rua!"),
-    number: z.number().min(1, "Digite o número da sua casa!"),
-    neiborhood: z.string().min(1, "Digite o bairro da sua casa!"),
+    number: z.string().min(1, "Digite o número da sua casa!"),
+    district: z.string().min(1, "Digite o bairro da sua casa!"),
     complement: z.string(),
     city: z.string().min(1, "Digite o nome da sua cidade!"),
     state: z.string().min(2, "Digite a sigla do seu estado!"),
