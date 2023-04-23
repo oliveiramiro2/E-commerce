7aecfaaefede9213a68dc8405a3b2dcea2e31d69
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-23 16:29:04 -0300

    added props to function login make edit profile
    add interface

--- src/components/LoginAndRegister/interfaces/loginValid.ts
+++ src/components/LoginAndRegister/interfaces/loginValid.ts
@@ -1,10 +1,13 @@
+import { ILoginData } from "@/interface";
 import { formProps } from "./register";
 
 export interface IloginValid {
     data: formProps;
     setShowIconLoading: Function;
     setAllUserData: Function;
+    allUserData: ILoginData;
     setLogined: Function;
     push: Function;
     paramRedirect(): string;
+    editProfile: boolean;
 }
