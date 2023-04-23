cb52e5c9eaeb23fb9f383f532380da8f56cc4feb
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-23 13:17:56 -0300

    added a new link to private routes
    add constant

--- src/constants/appRoutes.ts
+++ src/constants/appRoutes.ts
@@ -2,7 +2,8 @@ export const APP_ROUTES = {
     private: {
         products: '/produtos',
         cart: '/carrinho',
-        buy: '/comprar_produto'
+        buy: '/comprar_produto',
+        editProfile: '/editar_perfil',
     },
     public: {
         home: '/',
