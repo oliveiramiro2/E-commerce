630989972fab2077c2bb150530af5fe80b604b28
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-23 14:33:15 -0300

    added title of page and pass prop edit profile
    add component

--- src/templates/EditUser/editUser.tsx
+++ src/templates/EditUser/editUser.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 
 import { DefaultTemplate } from "../default";
 import { LoginAndRegister } from "@/components";
 
 export const EditUserTemlate: React.FC = () => {
+    useEffect(() => {
+        document.title = "RM E-commerce - Editar perfil";
+    }, []);
+
     return (
         <DefaultTemplate>
-            <LoginAndRegister registerComponent />
+            <LoginAndRegister registerComponent editProfile />
         </DefaultTemplate>
     );
 };
