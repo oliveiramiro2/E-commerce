324be90ce7048f3c4e000d8e26179e274003b563
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-23 13:10:52 -0300

    added page to edit profile
    add component

---
+++ src/templates/EditUser/editUser.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { DefaultTemplate } from "../default";
+
+export const EditUser: React.FC = () => {
+    return (
+        <DefaultTemplate>
+            <div>editUser</div>
+        </DefaultTemplate>
+    );
+};
