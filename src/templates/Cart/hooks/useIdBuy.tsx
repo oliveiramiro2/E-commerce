74d2cbc7647d81f1498a6e34198755c38ea91652
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-23 01:22:29 -0300

    added hooks to remove price of total
    add hook

--- src/templates/Cart/hooks/useIdBuy.tsx
+++ src/templates/Cart/hooks/useIdBuy.tsx
@@ -5,11 +5,17 @@ import { useState } from "react";
 export const useIdBuy = () => {
     const [buyId, setBuyId] = useState<number | undefined>();
     const [buyAll, setBuyAll] = useState<boolean>(false);
+    const [priceBuying, setPriceBuying] = useState<number>(0);
+    const [numberItems, setNumberItems] = useState<number>(0);
 
     return {
         buyId,
         setBuyId,
         buyAll,
         setBuyAll,
+        priceBuying,
+        setPriceBuying,
+        numberItems,
+        setNumberItems,
     };
 };
