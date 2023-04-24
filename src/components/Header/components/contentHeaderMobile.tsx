ff6f9999d6996f1329281719946619d9f44e0cfa
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-24 14:41:36 -0300

    added link to admin go to CRUD product
    add component

--- src/components/Header/components/contentHeaderMobile.tsx
+++ src/components/Header/components/contentHeaderMobile.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from "react";
 import Link from "next/link";
 import { FaHome, FaShoppingBag, FaShoppingCart } from "react-icons/fa";
-import { BiLogIn } from "react-icons/bi";
+import { BiCartAdd, BiLogIn } from "react-icons/bi";
 import clsx from "clsx";
 
 import { tiro } from "@/functions/fonts";
@@ -9,7 +9,8 @@ import { UserDataContext } from "@/contexts/userDataLogin";
 import { useRedirect } from "@/hooks";
 
 export const ContentHeaderMobile: React.FC = () => {
-    const { logined, setRedirectOnLogin } = useContext(UserDataContext);
+    const { logined, setRedirectOnLogin, allUserData } =
+        useContext(UserDataContext);
     const { push } = useRedirect();
 
     return (
@@ -28,41 +29,67 @@ export const ContentHeaderMobile: React.FC = () => {
                             Home
                         </Link>
                     </li>
-                    <li className="border-b w-full border-gray-800 pt-3 pb-5 pl-5">
-                        <button
-                            type="button"
-                            className={`text-pallet-white w-24 flex items-center gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
-                            onClick={() => {
-                                if (!logined)
-                                    setRedirectOnLogin("redirecionar=produtos");
-                                push("/produtos");
-                            }}
-                        >
-                            <FaShoppingBag className="relative bottom-[2px]" />{" "}
-                            Comprar
-                        </button>
-                    </li>
-                    <li
-                        className={clsx(
-                            "w-full border-gray-800 pt-3 pb-5 pl-5",
-                            {
-                                "border-b": !logined,
-                            }
-                        )}
-                    >
-                        <button
-                            type="button"
-                            className={`text-pallet-white flex items-center w-24 gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
-                            onClick={() => {
-                                if (!logined)
-                                    setRedirectOnLogin("redirecionar=carrinho");
-                                push("/carrinho");
-                            }}
-                        >
-                            <FaShoppingCart className="relative bottom-[2px]" />{" "}
-                            Carrinho
-                        </button>
-                    </li>
+                    {allUserData.role === "customer" ? (
+                        <>
+                            <li className="border-b w-full border-gray-800 pt-3 pb-5 pl-5">
+                                <button
+                                    type="button"
+                                    className={`text-pallet-white w-24 flex items-center gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                                    onClick={() => {
+                                        if (!logined)
+                                            setRedirectOnLogin(
+                                                "redirecionar=produtos"
+                                            );
+                                        push("/produtos");
+                                    }}
+                                >
+                                    <FaShoppingBag className="relative bottom-[2px]" />{" "}
+                                    Comprar
+                                </button>
+                            </li>
+                            <li
+                                className={clsx(
+                                    "w-full border-gray-800 pt-3 pb-5 pl-5",
+                                    {
+                                        "border-b": !logined,
+                                    }
+                                )}
+                            >
+                                <button
+                                    type="button"
+                                    className={`text-pallet-white flex items-center w-24 gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                                    onClick={() => {
+                                        if (!logined)
+                                            setRedirectOnLogin(
+                                                "redirecionar=carrinho"
+                                            );
+                                        push("/carrinho");
+                                    }}
+                                >
+                                    <FaShoppingCart className="relative bottom-[2px]" />{" "}
+                                    Carrinho
+                                </button>
+                            </li>
+                        </>
+                    ) : (
+                        <li className="border-b w-full border-gray-800 pt-3 pb-5 pl-5">
+                            <button
+                                type="button"
+                                className={`text-pallet-white w-24 flex items-center gap-x-2 font-medium text-md hover:border-b hover:transition-colors hover:drop-shadow-xl border-pallet-blue ${tiro.className}`}
+                                onClick={() => {
+                                    if (
+                                        !logined ||
+                                        allUserData.role === "customer"
+                                    )
+                                        push("/");
+                                    push("/gerenciar_produtos");
+                                }}
+                            >
+                                <BiCartAdd className="relative bottom-[2px]" />{" "}
+                                Gerenciar produtos
+                            </button>
+                        </li>
+                    )}
                     {!logined && (
                         <button
                             className="bg-pallet-orange hover:bg-[#ff9748] transition-colors shadow shadow-pallet-white h-5 p-20 mt-2 mb-2 pt-4 pb-4 rounded-lg flex justify-center items-center"
