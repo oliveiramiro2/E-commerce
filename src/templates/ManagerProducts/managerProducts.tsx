5f44564b293b37605a7daad7ba8aec75e2fdbf36
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-24 10:27:40 -0300

    added title of page and use client into template
    add template

--- src/templates/ManagerProducts/managerProducts.tsx
+++ src/templates/ManagerProducts/managerProducts.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 
 import { DefaultTemplate } from "../default";
 
 export const ManagerProductsTemplate: React.FC = () => {
+    useEffect(() => {
+        document.title = "RM E-commerce - Gerenciar produtos";
+    }, []);
+
     return (
         <DefaultTemplate>
-            <div>managerProducts</div>
+            <section className="w-screen min-h-[72vh] bg-gray-100 flex flex-col items-center">
+                managerProducts
+            </section>
         </DefaultTemplate>
     );
 };
