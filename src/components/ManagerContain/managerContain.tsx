8e2936a63084a019e7196fb833703fc201fe5ce1
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-24 19:01:08 -0300

    added layout of table of manager
    add component

--- src/components/ManagerContain/managerContain.tsx
+++ src/components/ManagerContain/managerContain.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import React from "react";
+import { SlPencil } from "react-icons/sl";
+import { FaRegTrashAlt } from "react-icons/fa";
 
 import { IProps } from "./interface";
 import { useData } from "./hook";
+import { tiro, arnekG } from "@/functions/fonts";
 
 export const ManagerContain: React.FC<IProps> = ({
     dataCategory,
@@ -12,11 +15,53 @@ export const ManagerContain: React.FC<IProps> = ({
     const { data } = useData(dataProduct, dataCategory);
 
     return (
-        <section className="w-[97vw] bg-white mt-5 mb-10 p-2 self-center rounded-lg border border-gray-300">
-            {data.map(item => (
-                <p key={item.id}>{item.name}</p>
-            ))}
+        <div>
+            <table className="rounded-md border-separate border-tools-table-outline w-[97vw] bg-white mt-5 mb-10 self-center border border-blue-200">
+                <thead>
+                    <tr className="h-14">
+                        <th
+                            className={`font-bold text-center ${tiro.className}`}
+                        >
+                            Nome do produto
+                        </th>
+                        <th
+                            className={`font-bold text-center border-l border-r border-blue-200 ${tiro.className}`}
+                        >
+                            Editar
+                        </th>
+                        <th
+                            className={`font-bold text-center ${tiro.className}`}
+                        >
+                            Excluir
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map(item => (
+                        <tr
+                            key={item.id}
+                            className="h-10 rounded-sm odd:bg-slate-100 border border-blue-200 border-separate border-tools-table-outline bg-white"
+                        >
+                            <td
+                                className={`border-t border-blue-200 pl-2 font-medium ${arnekG.className}`}
+                            >
+                                {item.name}
+                            </td>
+                            <td className="text-center border-t border-l border-r border-blue-200">
+                                <button type="button">
+                                    <SlPencil color="#e68a00" size={20} />
+                                </button>
+                            </td>
+                            <td className="border-t border-blue-200 text-center">
+                                <button type="button">
+                                    <FaRegTrashAlt color="#ff6666" size={20} />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
             <p>paginação</p>
-        </section>
+        </div>
     );
 };
