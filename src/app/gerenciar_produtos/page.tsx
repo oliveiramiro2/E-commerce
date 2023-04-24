9a72de8cf9c5ac41401178e5b04c676c47fc7949
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-24 09:46:02 -0300

    added page to admin manager products
    add page

---
+++ src/app/gerenciar_produtos/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { ManagerProductsTemplate } from "@/templates";
+
+const ManagerProducts: React.FC = () => <ManagerProductsTemplate />;
+
+export default ManagerProducts;
