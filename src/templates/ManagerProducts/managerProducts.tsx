5a15c89b142880b54c9e694306f446b1dba3ad1e
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-24 11:44:43 -0300

    added title of page
    add template

--- src/templates/ManagerProducts/managerProducts.tsx
+++ src/templates/ManagerProducts/managerProducts.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect } from "react";
 
 import { DefaultTemplate } from "../default";
+import { oswald } from "@/functions/fonts";
 
 export const ManagerProductsTemplate: React.FC = () => {
     useEffect(() => {
@@ -12,7 +13,13 @@ export const ManagerProductsTemplate: React.FC = () => {
     return (
         <DefaultTemplate>
             <section className="w-screen min-h-[72vh] bg-gray-100 flex flex-col items-center">
-                managerProducts
+                <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex flex-col justify-center mb-3 mt-6">
+                    <h3
+                        className={`font-black text-3xl self-center max-lg:right-0 ${oswald.className}`}
+                    >
+                        Gerenciar produtos
+                    </h3>
+                </div>
             </section>
         </DefaultTemplate>
     );
