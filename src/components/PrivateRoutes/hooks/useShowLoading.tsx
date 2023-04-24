670b0488e47a2582410d4ebaf87295bbcfd94451
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-24 11:41:45 -0300

    added true with as default value on hook
    add hook

--- src/components/PrivateRoutes/hooks/useShowLoading.tsx
+++ src/components/PrivateRoutes/hooks/useShowLoading.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 
 export const useShowLoading = () => {
-    const [showLoading, setShowLoading] = useState<boolean>(false);
+    const [showLoading, setShowLoading] = useState<boolean>(true);
 
     return {
         showLoading,
