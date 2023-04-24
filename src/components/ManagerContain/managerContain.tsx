ff36fe38413e343472548cb5155525fd569cfed5
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-24 15:25:48 -0300

    added a simple layout of contain
    add component

--- src/components/ManagerContain/managerContain.tsx
+++ src/components/ManagerContain/managerContain.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+"use client";
 
-export const ManagerContain: React.FC = () => {
-  return (
-    <div>managerContain</div>
-  )
-}
+import React from "react";
+
+import { IProps } from "./interface";
+import { useData } from "./hook";
+
+export const ManagerContain: React.FC<IProps> = ({
+    dataCategory,
+    dataProduct,
+}) => {
+    const { data } = useData(dataProduct, dataCategory);
+
+    return (
+        <section className="w-screen h-52 bg-white border-gray-200">
+            {data.map(item => (
+                <p key={item.id}>{item.name}</p>
+            ))}
+            <p>paginação</p>
+        </section>
+    );
+};
