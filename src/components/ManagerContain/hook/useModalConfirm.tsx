650edf5815d94e18693e5da8080f84e84459efcb
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-25 16:28:12 -0300

    added a hook to show loading into modal
    add hook

--- src/components/ManagerContain/hook/useModalConfirm.tsx
+++ src/components/ManagerContain/hook/useModalConfirm.tsx
@@ -4,9 +4,12 @@ import { useState } from "react";
 
 export const useModalConfirm = () => {
     const [showModalConfirm, setShowModalConfirm] = useState<boolean>(false);
+    const [showLoading, setShowLoading] = useState<boolean>(false);
 
     return {
         showModalConfirm,
+        showLoading,
+        setShowLoading,
         setShowModalConfirm,
     };
 };
