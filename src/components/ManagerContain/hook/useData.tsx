e5284168eb7978b1d8fa8786838e61bbb8e3efb2
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-25 07:44:04 -0300

    added logic to app make pagination into hook page
    add hook

--- src/components/ManagerContain/hook/useData.tsx
+++ src/components/ManagerContain/hook/useData.tsx
@@ -11,40 +11,54 @@ export const useData = (
     numberItemsPagination: number
 ) => {
     const [data, setData] = useState<IDataTable[]>([]);
+    const [allData, setAllData] = useState<IDataTable[]>([]);
     const [numPage, setNumPage] = useState<number>(1);
+    const [numItems, setNumItems] = useState<number>(numberItemsPagination);
     const [page, setPag] = useState<number>(1);
 
     const auxData: IDataTable[] = [];
-    let numItems: number = 1;
+    let auxNumItems: number = 1;
     useEffect(() => {
         if (dataProduct.length > 1) {
-            numItems = dataProduct.length;
-            setNumPage(Math.ceil(numItems / numberItemsPagination));
+            auxNumItems = dataProduct.length;
+            setNumPage(Math.ceil(auxNumItems / numberItemsPagination));
 
             dataProduct.forEach(item => {
                 const aux: IDataTable = { name: item.title, id: item.id };
                 auxData.push(aux);
             });
-            setData(auxData);
+            setAllData(auxData);
         } else {
-            numItems = dataCategory.length;
-            setNumPage(Math.ceil(numItems / numberItemsPagination));
+            auxNumItems = dataCategory.length;
+            setNumPage(Math.ceil(auxNumItems / numberItemsPagination));
 
             dataCategory.forEach(item => {
                 const aux: IDataTable = { name: item.name, id: item.id };
                 auxData.push(aux);
             });
-            setData(auxData);
+            setAllData(auxData);
         }
     }, []);
+
+    useEffect(() => {
+        const aux: number = numItems * page - 1;
+        const auxItems: any = [];
+        allData.forEach((item, index) => {
+            if (aux - index >= 0 && index > aux - numItems) {
+                auxItems.push(item);
+            }
+        });
+        setData(auxItems);
+    }, [allData, page, numPage]);
 
     return {
         data,
         handleNewData: (name: string, id: number) =>
             setData([...data, { name, id }]),
         numPage,
-        handleNumPage: (num: number) => Math.ceil(numItems / num),
+        handleNumPage: (num: number) => Math.ceil(auxNumItems / num),
         page,
+        handleItemPerPage: (num: number) => setNumItems(num),
         handlePagPerIndex: (index: number) => setPag(index),
         handlePagMore: () => {
             if (page === numPage) return;
