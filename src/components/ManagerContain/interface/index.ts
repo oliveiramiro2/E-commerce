d87e6a75be453326417221d61cec32addd10969d
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-25 16:27:52 -0300

    added interface to modal confirm delete component
    add interface

--- src/components/ManagerContain/interface/index.ts
+++ src/components/ManagerContain/interface/index.ts
@@ -1,3 +1,4 @@
 export type { IProps, IDataTable } from './propsManagerContain';
 export type { IPropsPagination } from './propsPagination';
 export type { IOptions } from './options';
+export type { IPropsConfirmDelete } from './modalConfirm';
---
+++ src/components/ManagerContain/interface/modalConfirm.ts
@@ -0,0 +1,5 @@
+export interface IPropsConfirmDelete {
+    handleClose: Function;
+    showLoading: Function;
+    loading: boolean;
+}
