18318263f5e01e5ca46d47928bcf3fc91fdca054
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-25 16:47:40 -0300

    added a hook to save the id of item to delete this
    add hook

--- src/components/ManagerContain/hook/useModalConfirm.tsx
+++ src/components/ManagerContain/hook/useModalConfirm.tsx
@@ -5,11 +5,14 @@ import { useState } from "react";
 export const useModalConfirm = () => {
     const [showModalConfirm, setShowModalConfirm] = useState<boolean>(false);
     const [showLoading, setShowLoading] = useState<boolean>(false);
+    const [idItem, setIdItem] = useState<number>(0);
 
     return {
         showModalConfirm,
         showLoading,
         setShowLoading,
         setShowModalConfirm,
+        idItem,
+        setIdItem,
     };
 };
