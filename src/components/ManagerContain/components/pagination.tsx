a9e0f0a8b29bad07e3bec78fa1d3778d531b30e4
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-25 06:10:19 -0300

    added a simple layout of component pagination
    left test it
    
    add component

--- src/components/ManagerContain/components/pagination.tsx
+++ src/components/ManagerContain/components/pagination.tsx
@@ -1,7 +1,67 @@
-import React from 'react'
+import React from "react";
+import { TiChevronLeft, TiChevronRight } from "react-icons/ti";
 
-export const Pagination: React.FC = () => {
-  return (
-    <div>pagination</div>
-  )
-}
+export const Pagination: React.FC<{ page: number; numPage: number }> = ({
+    page,
+    numPage,
+}) => (
+    <div className="flex items-center mb-10">
+        <button
+            type="button"
+            /* className={clsx("h-max", {
+                            invisible: pagination <= 1,
+                        })}
+                        onClick={() => handlePagination(false)}
+                        disabled={pagination <= 1} */
+        >
+            <div className="bg-pallet-orange h-max rounded-md mr-2 p-2 hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange">
+                <TiChevronLeft color="#fff" />
+            </div>
+        </button>
+        <div className="flex gap-x-1">
+            {page - 2 > 0 && (
+                <button
+                    className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
+                    type="button"
+                >
+                    {page - 2}
+                </button>
+            )}
+            {page - 1 > 0 && (
+                <button
+                    className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
+                    type="button"
+                >
+                    {page - 1}
+                </button>
+            )}
+            <button
+                className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
+                type="button"
+            >
+                {page}
+            </button>
+            {page + 2 <= numPage && (
+                <button
+                    className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
+                    type="button"
+                >
+                    {page + 2}
+                </button>
+            )}
+            {page + 1 <= numPage && (
+                <button
+                    className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
+                    type="button"
+                >
+                    {page + 1}
+                </button>
+            )}
+        </div>
+        <button type="button" className="h-max">
+            <div className="bg-pallet-orange h-max rounded-md ml-2 p-2 hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange">
+                <TiChevronRight color="#fff" />
+            </div>
+        </button>
+    </div>
+);
