f3e63b14de13c417f9df67ee1000d5ce2384f028
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-25 16:44:23 -0300

    added a return as response to make action into modal
    adjust function

--- src/components/ManagerContain/function/delete.ts
+++ src/components/ManagerContain/function/delete.ts
@@ -1,11 +1,9 @@
-import { notify } from "@/functions/notifications"
 import { deleteProduct } from "@/services/api"
 
-export const deleteItem = async (product: boolean, id: number): Promise<void> => {
+export const deleteItem = async (product: boolean, id: number): Promise<boolean> => {
     if (product) {
         const data = await deleteProduct(id)
-        if (data) {
-            notify("success", "Sucesso,", "Produto deletado com sucesso!")
-        }
+        return data
     }
+    return false
 }
