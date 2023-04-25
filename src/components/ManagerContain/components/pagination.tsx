f433fa13c27c0411f349baf7fac6c8c514f9efb3
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-25 06:19:37 -0300

    added logic to make pagination left test it
    add component

--- src/components/ManagerContain/components/pagination.tsx
+++ src/components/ManagerContain/components/pagination.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { TiChevronLeft, TiChevronRight } from "react-icons/ti";
+import clsx from "clsx";
 
 import { IPropsPagination } from "../interface";
 
 export const Pagination: React.FC<IPropsPagination> = ({
     page,
     numPage,
+    handlePagMinus,
+    handlePagMore,
+    handlePagPerIndex,
 }) => (
     <div className="flex items-center mb-10">
         <button
             type="button"
-            /* className={clsx("h-max", {
-                            invisible: pagination <= 1,
-                        })}
-                        onClick={() => handlePagination(false)}
-                        disabled={pagination <= 1} */
+            className={clsx("h-max", {
+                invisible: page <= 1,
+            })}
+            onClick={() => handlePagMinus()}
+            disabled={page <= 1}
         >
             <div className="bg-pallet-orange h-max rounded-md mr-2 p-2 hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange">
                 <TiChevronLeft color="#fff" />
@@ -25,6 +29,7 @@ export const Pagination: React.FC<IPropsPagination> = ({
                 <button
                     className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
                     type="button"
+                    onClick={() => handlePagPerIndex(page - 2)}
                 >
                     {page - 2}
                 </button>
@@ -33,6 +38,7 @@ export const Pagination: React.FC<IPropsPagination> = ({
                 <button
                     className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
                     type="button"
+                    onClick={() => handlePagPerIndex(page - 1)}
                 >
                     {page - 1}
                 </button>
@@ -40,27 +46,37 @@ export const Pagination: React.FC<IPropsPagination> = ({
             <button
                 className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
                 type="button"
+                disabled
             >
                 {page}
             </button>
-            {page + 2 <= numPage && (
-                <button
-                    className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
-                    type="button"
-                >
-                    {page + 2}
-                </button>
-            )}
             {page + 1 <= numPage && (
                 <button
                     className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
                     type="button"
+                    onClick={() => handlePagPerIndex(page + 1)}
                 >
                     {page + 1}
                 </button>
             )}
+            {page + 2 <= numPage && (
+                <button
+                    className="p-1 bg-pallet-orange rounded-md hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange"
+                    type="button"
+                    onClick={() => handlePagPerIndex(page + 2)}
+                >
+                    {page + 2}
+                </button>
+            )}
         </div>
-        <button type="button" className="h-max">
+        <button
+            type="button"
+            className={clsx("h-max", {
+                invisible: page === numPage,
+            })}
+            onClick={() => handlePagMore()}
+            disabled={page === numPage}
+        >
             <div className="bg-pallet-orange h-max rounded-md ml-2 p-2 hover:bg-[#ff9748] transition-colors shadow-md shadow-pallet-orange">
                 <TiChevronRight color="#fff" />
             </div>
