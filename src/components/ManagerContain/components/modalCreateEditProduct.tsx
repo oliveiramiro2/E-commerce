15386c938c694d8ca9b1c429bd1b8dc4fc514a24
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 08:48:22 -0300

    added animation on show modal and title
    add component

--- src/components/ManagerContain/components/modalCreateEditProduct.tsx
+++ src/components/ManagerContain/components/modalCreateEditProduct.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useLayoutEffect } from "react";
+import { gsap } from "gsap";
+
+import { oswald } from "@/functions/fonts";
 
 export const ModalCreateEditProduct: React.FC = () => {
-  return (
-    <div>modalCreateEditProduct</div>
-  )
-}
+    useLayoutEffect(() => {
+        gsap.from(".contain-modal-product", {
+            scale: 0,
+            opacity: 0,
+            ease: "slow",
+        });
+    }, []);
+
+    return (
+        <div className="contain-modal-product">
+            <p
+                className={`font-black text-xl text-center self-center max-lg:right-0 ${oswald.className}`}
+            >
+                Deseja excluir o produto?
+            </p>
+        </div>
+    );
+};
