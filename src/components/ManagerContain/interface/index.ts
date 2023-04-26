a2902fcd07d81947feb3c938372cdaac2fb4af83
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 10:38:07 -0300

    added props to modal add and edit product
    add interface

--- src/components/ManagerContain/interface/index.ts
+++ src/components/ManagerContain/interface/index.ts
@@ -2,3 +2,4 @@ export type { IProps, IDataTable } from './propsManagerContain';
 export type { IPropsPagination } from './propsPagination';
 export type { IOptions } from './options';
 export type { IPropsConfirmDelete } from './modalConfirm';
+export type { IPropsAddEditProduct } from './modalAddEdit';
