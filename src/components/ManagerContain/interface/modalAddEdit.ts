968b3bcdb8ae9029a1288dd7d10637fb02a69951
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 10:55:00 -0300

    added hook to control inputs and your nedded function to change values
    add hook

--- src/components/ManagerContain/interface/modalAddEdit.ts
+++ src/components/ManagerContain/interface/modalAddEdit.ts
@@ -1,3 +1,11 @@
 export interface IPropsAddEditProduct {
     add: boolean;
 }
+
+export interface IDataProduct {
+    title: string;
+    description: string;
+    price: number;
+    category: number;
+    trySendErro: boolean;
+}
