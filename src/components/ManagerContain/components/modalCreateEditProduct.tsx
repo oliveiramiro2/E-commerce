e67364ea8685ef4eaad7ac51d4c678de7295fb74
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 10:06:47 -0300

    added layout of elements of form
    add component

--- src/components/ManagerContain/components/modalCreateEditProduct.tsx
+++ src/components/ManagerContain/components/modalCreateEditProduct.tsx
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect } from "react";
 import { gsap } from "gsap";
 
-import { oswald } from "@/functions/fonts";
+import { arnekG, oswald, tiro } from "@/functions/fonts";
 
 export const ModalCreateEditProduct: React.FC = () => {
     useLayoutEffect(() => {
@@ -21,18 +21,42 @@ export const ModalCreateEditProduct: React.FC = () => {
             </p>
             <form className="w-full h-[40vh] flex flex-col justify-around items-center pl-[5vw] pr-[5vw]">
                 <div className="w-full flex justify-between">
-                    <input type="text" placeholder="Nome" />
-                    <input type="text" placeholder="Descrição" />
+                    <input
+                        type="text"
+                        placeholder="Nome"
+                        className={`outline-none border-[1.5px] border-pallet-purple p-1 pl-1 rounded-lg ${tiro.className}`}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Descrição"
+                        className={`outline-none border-[1.5px] border-pallet-purple p-1 pl-1 rounded-lg ${tiro.className}`}
+                    />
                 </div>
                 <div className="w-full flex justify-between">
-                    <input type="text" placeholder="Preço" />
-                    <select>
+                    <input
+                        type="text"
+                        placeholder="Preço"
+                        className={`outline-none border-[1.5px] border-pallet-purple p-1 pl-1 rounded-lg ${tiro.className}`}
+                    />
+                    <select
+                        className={`select text-center outline-none border-2 border-pallet-purple p-1 pl-1 rounded-lg ${tiro.className}`}
+                    >
                         <option value="ok">test</option>
                     </select>
                 </div>
                 <div className="w-full flex justify-around">
-                    <button type="button">Adicionar</button>
-                    <button type="button">Cancelar</button>
+                    <button
+                        type="button"
+                        className={`bg-green-500 w-[40%] rounded-md p-2 pb-1 hover:bg-green-400 transition-colors shadow-md shadow-gray-400 text-white font-bold tracker ${arnekG.className}`}
+                    >
+                        Adicionar
+                    </button>
+                    <button
+                        type="button"
+                        className={`bg-pallet-orange w-[40%] rounded-md p-2 pb-1 hover:bg-[#ff9748] transition-colors shadow-md shadow-gray-400 text-white font-bold ${arnekG.className}`}
+                    >
+                        Cancelar
+                    </button>
                 </div>
             </form>
         </div>
