bad2ed3d5aaedf4b26b56f2531a91f9f3e86491a
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 11:03:39 -0300

    added the props to add new product
    add interface

--- src/components/ManagerContain/interface/modalAddEdit.ts
+++ src/components/ManagerContain/interface/modalAddEdit.ts
@@ -1,11 +1,14 @@
-export interface IPropsAddEditProduct {
-    add: boolean;
-}
-
 export interface IDataProduct {
     title: string;
     description: string;
     price: number;
     category: number;
     trySendErro: boolean;
+}
+
+export interface IPropsAddEditProduct {
+    add: boolean;
+    data: IDataProduct;
+    handleSingle: Function;
+    cleanData: Function;
 }
