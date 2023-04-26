d8fb40091f6f41793e432d39f51e64a4b677be7f
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 18:04:34 -0300

    added logic to create user and return if work it
    add function

--- src/components/ManagerContain/function/createProduct.ts
+++ src/components/ManagerContain/function/createProduct.ts
@@ -1,28 +1,54 @@
 import { notify } from "@/functions/notifications";
 import { IDataProduct } from "../interface";
+import { INewProductData } from "@/interface";
+import { addProduct } from "@/services/api";
 
-export const createProduct = (data: IDataProduct, handleData: Function) => {
+/* eslint-disable-next-line */
+export const createProduct = (data: IDataProduct, handleData: Function, clearProduct: Function) => {
     if (data.title === "") {
-        handleData({...data, trySendErro: true});
-        notify("warning", "Erro,", "Não foi possível fazer login preencha o nome do produto");
-        return
+        handleData({...data, trySendErro: true})
+        notify("warning", "Erro,", "Não foi possível fazer login preencha o nome do produto")
+        return false
     }
     if (data.description === "") {
-        handleData({...data, trySendErro: true});
-        notify("warning", "Erro,", "Não foi possível fazer login preencha a descrição do produto");
-        return
+        handleData({...data, trySendErro: true})
+        notify("warning", "Erro,", "Não foi possível fazer login preencha a descrição do produto")
+        return false
     }
     if (data.price === "") {
-        handleData({...data, trySendErro: true});
-        notify("warning", "Erro,", "Não foi possível fazer login preencha o preço do produto");
-        return
+        handleData({...data, trySendErro: true})
+        notify("warning", "Erro,", "Não foi possível fazer login preencha o preço do produto")
+        return false
     }
     if (data.category === 0) {
-        handleData({...data, trySendErro: true});
-        notify("warning", "Erro,", "Não foi possível fazer login preencha a categoria do produto");
-        return
+        handleData({...data, trySendErro: true})
+        notify("warning", "Erro,", "Não foi possível fazer login preencha a categoria do produto")
+        return false
     }
 
-    handleData({...data, trySendErro: false});
-    notify("success", "Sucesso,", "Produto foi cadastrado com sucesso!")
+    const dataForm: INewProductData = {
+        categoryId: data.category,
+        description: data.description,
+        price: Number(data.price),
+        title: data.title,
+        images: [
+            "https://loremflickr.com/320/240/product",
+            "https://loremflickr.com/320/240/product",
+            "https://loremflickr.com/320/240/product"
+        ]
+    }
+
+    addProduct(dataForm).then(value => {
+        if (value) {
+            handleData({...data, trySendErro: false});
+            clearProduct()
+            notify("success", "Sucesso,", "Produto foi cadastrado com sucesso!")
+            return true
+        }
+        notify("danger", "Erro desculpe,", "não foi possível cadastrar o produto!")
+        return false
+    }).catch(() => {
+        notify("danger", "Erro desculpe,", "não foi possível cadastrar o produto!")
+        return false
+    })
 }
