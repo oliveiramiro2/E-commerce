7c729feef1eb2e0dff4f22cc4509c82409cac198
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 10:56:18 -0300

    added interface to data of add and edit product
    add interface

--- src/components/ManagerContain/hook/useModalAddEditProduct.tsx
+++ src/components/ManagerContain/hook/useModalAddEditProduct.tsx
@@ -1,15 +1,48 @@
 "use client";
 
 import { useState } from "react";
+import { IDataProduct } from "../interface";
+
+const defaultValuesProduct: IDataProduct = {
+    title: "",
+    description: "",
+    price: 0,
+    category: 0,
+    trySendErro: false,
+}
 
 export const useModalAddEditProduct = () => {
     const [openModalProduct, setOpenModalProduct] = useState<boolean>(false);
     const [addNew, setAddNew] = useState<boolean>(false);
+    const [dataProduct, setDataProduct] =
+        useState<IDataProduct>(defaultValuesProduct);
 
     return {
         openModalProduct,
         addNew,
+        dataProduct,
         setOpenModalProduct,
         setAddNew,
-    }
-}
+        handleSingleData: (value: string, index: number) => {
+            switch (index) {
+                case 0:
+                    setDataProduct({...dataProduct, title: value})
+                    break;
+                case 1:
+                    setDataProduct({...dataProduct, description: value})
+                    break;
+                case 2:
+                    setDataProduct({...dataProduct, price: Number(value)})
+                    break;
+                case 3:
+                    setDataProduct({...dataProduct, category: Number(value)})
+                    break;
+                default:
+                    setDataProduct({ ...dataProduct, trySendErro: true });
+                    break;
+            }
+        },
+        handleEditData: (value: IDataProduct) => setDataProduct(value),
+        handleCleanData: () => setDataProduct(defaultValuesProduct)
+    };
+};
