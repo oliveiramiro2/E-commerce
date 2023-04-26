396ef0ae3e33772a0246e83ba9df41cbeedf43a1
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-26 09:51:16 -0300

    added layout of father contains of forms and contain
    add component

--- src/components/ManagerContain/components/modalCreateEditProduct.tsx
+++ src/components/ManagerContain/components/modalCreateEditProduct.tsx
@@ -13,24 +13,24 @@ export const ModalCreateEditProduct: React.FC = () => {
     }, []);
 
     return (
-        <div className="contain-modal-product">
+        <div className="contain-modal-product flex flex-col justify-center min-h-[50vh]">
             <p
                 className={`font-black text-xl text-center self-center max-lg:right-0 ${oswald.className}`}
             >
                 Adicionar produto
             </p>
-            <form>
-                <div className="flex">
+            <form className="w-full h-[40vh] flex flex-col justify-around items-center pl-[5vw] pr-[5vw]">
+                <div className="w-full flex justify-between">
                     <input type="text" placeholder="Nome" />
                     <input type="text" placeholder="Descrição" />
                 </div>
-                <div className="flex">
+                <div className="w-full flex justify-between">
                     <input type="text" placeholder="Preço" />
                     <select>
                         <option value="ok">test</option>
                     </select>
                 </div>
-                <div>
+                <div className="w-full flex justify-around">
                     <button type="button">Adicionar</button>
                     <button type="button">Cancelar</button>
                 </div>
