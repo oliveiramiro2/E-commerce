f3bcabd6ff5f402b2208c3cffb5da164708a22b1
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-27 08:15:46 -0300

    added folder and file to admin manager categories
    add page

---
+++ src/app/gerenciar_categorias/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { ManagerCategoriesTemplate } from "@/templates";
+
+const ManagerCategories: React.FC = () => <ManagerCategoriesTemplate />;
+
+export default ManagerCategories;
