26e3adf89034658994ae918c26dca2d28ec5a6d9
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-27 08:46:29 -0300

    added logic into function to make request for delete category
    add function

--- src/components/ManagerContain/function/delete.ts
+++ src/components/ManagerContain/function/delete.ts
@@ -1,8 +1,12 @@
-import { deleteProduct } from "@/services/api"
+import { deleteCategory, deleteProduct } from "@/services/api"
 
 export const deleteItem = async (product: boolean, id: number): Promise<boolean> => {
     if (product) {
         const data = await deleteProduct(id)
+        return data
+    }
+    if (!product) {
+        const data = await deleteCategory(id)
         return data
     }
     return false
