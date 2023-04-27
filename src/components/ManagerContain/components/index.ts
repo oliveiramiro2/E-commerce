1b959901c11e7767ed5140d79b547ce864201dbf
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-27 08:58:11 -0300

    added file to create and edit categories
    add component

--- src/components/ManagerContain/components/index.ts
+++ src/components/ManagerContain/components/index.ts
@@ -1,3 +1,4 @@
 export { Pagination } from './pagination';
 export { ModalConfirmDelete } from './modalConfirmDelete';
 export { ModalCreateEditProduct } from './modalCreateEditProduct';
+export { ModalCreateEditCategory } from './modalCreateEditCategory';
---
+++ src/components/ManagerContain/components/modalCreateEditCategory.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from 'react'
+
+export const ModalCreateEditCategory: React.FC = () => {
+  return (
+    <div>modalCreateEditCategory</div>
+  )
+}
