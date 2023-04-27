a640803cc670843b645a75c9067ab36661972344
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-27 07:34:45 -0300

    added component skeleton manager table into template manager product
    add template

--- src/templates/ManagerProducts/managerProducts.tsx
+++ src/templates/ManagerProducts/managerProducts.tsx
@@ -2,11 +2,10 @@
 
 import React, { useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
-import Skeleton from "react-loading-skeleton";
 
 import { DefaultTemplate } from "../default";
 import { oswald } from "@/functions/fonts";
-import { ManagerContain } from "@/components";
+import { ManagerContain, SkeletonManagetTable } from "@/components";
 import { IDataApi } from "@/interface";
 import { allProductsWithoutParam } from "@/services/api";
 
@@ -21,24 +20,7 @@ export const ManagerProductsTemplate: React.FC = () => {
         document.title = "RM E-commerce - Gerenciar produtos";
     }, []);
 
-    if (isLoading)
-        return (
-            <DefaultTemplate>
-                <section className="w-screen min-h-[72vh] bg-gray-100 flex flex-col items-center">
-                    <div className="w-[45%] max-xl:w-[60%] max-lg:w-[70%] max-md:w-[80%] max-sm:w-[90%] flex flex-col justify-center mb-3 mt-6">
-                        <h3
-                            className={`font-black text-3xl self-center max-lg:right-0 ${oswald.className}`}
-                        >
-                            Gerenciar produtos
-                        </h3>
-                    </div>
-                    <div className="w-[97vw] bg-white rounded-md mt-5 mb-10 self-center border border-blue-200">
-                        <Skeleton count={1} className="w-full h-14" />
-                        <Skeleton count={6} className="w-full h-10" />
-                    </div>
-                </section>
-            </DefaultTemplate>
-        );
+    if (isLoading) return <SkeletonManagetTable />
 
     return (
         <DefaultTemplate>
