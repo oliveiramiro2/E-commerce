9b7a3a9c0e126a74afef0486333d0c856317a81c
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-27 10:10:15 -0300

    added interface to props add and edit category
    ad interface

--- src/components/ManagerContain/interface/index.ts
+++ src/components/ManagerContain/interface/index.ts
@@ -3,4 +3,4 @@ export type { IPropsPagination } from './propsPagination';
 export type { IOptions } from './options';
 export type { IPropsConfirmDelete } from './modalConfirm';
 export type { IPropsAddEditProduct, IDataProduct } from './modalAddEdit';
-export type { IDataCategory } from './modalAddEditCategory';
+export type { IDataCategory, IPropsAddEditCategory } from './modalAddEditCategory';
--- src/components/ManagerContain/interface/modalAddEditCategory.ts
+++ src/components/ManagerContain/interface/modalAddEditCategory.ts
@@ -2,3 +2,15 @@ export interface IDataCategory {
     name: string;
     trySendErro: boolean;
 }
+
+export interface IPropsAddEditCategory {
+    add: boolean;
+    requestIsLoading: boolean;
+    dataCategory: IDataCategory;
+    handleData: Function;
+    setRequestIsLoading: Function;
+    closeModal: Function;
+    editId: number;
+    setIdNewItem: Function;
+    /* setProductEdited: Function; */
+}
