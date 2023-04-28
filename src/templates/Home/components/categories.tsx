2728d137c59988c03d5a2e7e414ef2d38aa5a3d1
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-28 19:40:50 -0300

    add animation on categories component
    add animation

--- src/templates/Home/components/categories.tsx
+++ src/templates/Home/components/categories.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useQuery } from "@tanstack/react-query";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { oswald } from "@/functions/fonts";
 import { categories } from "@/services/api";
@@ -14,6 +16,33 @@ export const Categories: React.FC = () => {
         queryFn: () => categories(""),
         queryKey: ["allCategories"],
     });
+
+    gsap.registerPlugin(ScrollTrigger);
+    const refCategories = useRef(null);
+
+    useEffect(() => {
+        if (isLoading) return;
+        const element: any = refCategories.current;
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: element,
+                scrub: 2,
+                start: "bottom 75%",
+                end: "bottom 75%",
+            },
+        })
+            .from(element.querySelector("#homeBestOfferText"), {
+                y: -100,
+                opacity: 0,
+                scaleX: 0.2,
+            })
+            .from(".category", {
+                opacity: 0,
+                scaleY: 0,
+                ease: "slow",
+            });
+    }, [isLoading]);
 
     if (isLoading) {
         return (
@@ -45,8 +74,11 @@ export const Categories: React.FC = () => {
     }
 
     return (
-        <section className="w-screen flex flex-col justify-center items-center mb-5">
-            <div>
+        <section
+            ref={refCategories}
+            className="w-screen flex flex-col justify-center items-center mb-5"
+        >
+            <div id="homeBestOfferText">
                 <h2
                     className={`font-black text-3xl relative right-5 max-lg:right-0 mb-8 ${oswald.className}`}
                 >
