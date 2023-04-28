52cfd671d93676aa56becab676883d0b3a0416af
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-28 19:40:06 -0300

    added some class to set a default layout and class to animate this component
    add component

--- src/templates/Home/components/containCategory.tsx
+++ src/templates/Home/components/containCategory.tsx
@@ -7,27 +7,30 @@ import { UserDataContext } from "@/contexts/userDataLogin";
 
 export const ContainCategory: React.FC<{ data: ICategoryApi }> = ({ data }) => {
     const { push } = useRedirect();
-    const { logined, setRedirectOnLogin } = useContext(UserDataContext)
+    const { logined, setRedirectOnLogin } = useContext(UserDataContext);
 
     return (
         <button
             type="button"
             onClick={() => {
-                if (!logined) setRedirectOnLogin(`redirecionar=produtos&categoria=${data.id}`)
-                push(`produtos?categoria=${data.id}`)
+                if (!logined)
+                    setRedirectOnLogin(
+                        `redirecionar=produtos&categoria=${data.id}`
+                    );
+                push(`produtos?categoria=${data.id}`);
             }}
-            className="cursor-pointer"
+            className="category cursor-pointer"
         >
-            <div className="min-w-[100px] max-w-[120px] pl-2 pr-2 flex flex-col justify-between items-center rounded-lg pb-2 shadow-xl shadow-gray-500 bg-gradient-to-r to-pallet-orange via-pallet-white from-pallet-purple">
+            <div className="max-w-[120px] h-[150px] pl-2 pr-2 flex flex-col justify-between items-center rounded-sm pb-2 shadow-xl shadow-gray-400 bg-gradient-to-r to-[#f2f2f2] via-[#e6e6e6] from-[#d9d9d9]">
                 <p
-                    className={`font-medium text-center text-pallet-black first-letter:capitalize ${oswald.className}`}
+                    className={`font-medium max-w-[120px] h-5 text-center text-pallet-black first-letter:capitalize ${oswald.className}`}
                 >
                     {data.name}
                 </p>
                 <img
                     src={data.image}
                     alt="imagem categoria"
-                    className="w-auto h-20"
+                    className="w-auto h-[120px]"
                 />
             </div>
         </button>
