1b4f064790a2a92fb3a3ad44291591b82bbb7160
oliveiramiro2 <ramiromares95@gmail.com>
2023-04-28 14:51:33 -0300

    add animation on sign up home
    add animation

--- src/templates/Home/components/signUp.tsx
+++ src/templates/Home/components/signUp.tsx
@@ -1,24 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useEffect } from "react";
 import Link from "next/link";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 import { arnekG, oswald, tiro } from "@/functions/fonts";
 import { RegisterHomeContext } from "@/contexts/registerUserHome";
 
-
 export const SignUpHome: React.FC = () => {
     const { userData, setUserData } = useContext(RegisterHomeContext);
 
+    gsap.registerPlugin(ScrollTrigger);
+    const refSignUp = useRef(null);
+
+    useEffect(() => {
+        const element: any = refSignUp.current;
+
+        gsap.timeline({
+            scrollTrigger: {
+                trigger: element.querySelector("#homeSignUp"),
+                scrub: 1,
+                start: "center 55%",
+                end: "center 45%",
+            },
+        })
+            .from(element.querySelector("#homeSignUpText"), {
+                y: -100,
+                opacity: 0,
+                scaleX: 0.2,
+            })
+            .from(element.querySelector("#homeInputs"), {
+                xPercent: -100,
+                opacity: 0,
+                scaleX: 0.2,
+            })
+            .from(element.querySelector("#homeTextSignUp"), {
+                xPercent: 100,
+                opacity: 0,
+                scaleX: 0.2,
+            });
+    }, []);
+
     return (
-        <section className="w-screen min-h-[65vh] flex flex-col items-center pt-8">
-            <div>
+        <section
+            ref={refSignUp}
+            className="w-screen min-h-[65vh] flex flex-col items-center pt-8"
+        >
+            <div id="homeSignUp" className="w-full">
                 <h2
-                    className={`font-black text-3xl relative right-5 max-lg:right-0 ${oswald.className}`}
+                    id="homeSignUpText"
+                    className={`font-black text-3xl text-center max-lg:right-0 ${oswald.className}`}
                 >
                     Faça seu cadastro
                 </h2>
             </div>
             <div className="flex mt-8 justify-center gap-x-8 pl-5 pr-5 max-sm:flex-wrap-reverse max-sm:mb-8">
-                <div className="flex flex-col w-[30%] max-lg:w-[40%] justify-center items-end gap-y-5 max-sm:w-[90%] max-sm:items-center">
+                <div
+                    id="homeInputs"
+                    className="flex flex-col w-[30%] max-lg:w-[40%] justify-center items-end gap-y-5 max-sm:w-[90%] max-sm:items-center"
+                >
                     <input
                         type="email"
                         className={`min-w-[90%] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
@@ -33,7 +72,10 @@ export const SignUpHome: React.FC = () => {
                         className={`min-w-[90%] max-sm:w-[90%] outline-none border-[1.5px] border-pallet-purple p-3 pl-5 rounded-lg ${tiro.className}`}
                         placeholder="Digite sua senha"
                         onChange={e =>
-                            setUserData({ ...userData, password: e.target.value })
+                            setUserData({
+                                ...userData,
+                                password: e.target.value,
+                            })
                         }
                         defaultValue={userData.password}
                     />
@@ -44,7 +86,10 @@ export const SignUpHome: React.FC = () => {
                         <Link href="/cadastro">Registrar</Link>
                     </button>
                 </div>
-                <div className="p-10 w-[40%] max-sm:w-[80%] max-md:pl-2 max-md:pr-2">
+                <div
+                    id="homeTextSignUp"
+                    className="p-10 w-[40%] max-sm:w-[80%] max-md:pl-2 max-md:pr-2"
+                >
                     <p
                         className={`font-extrabold text-2xl max-lg:text-lg tracking-widest ${arnekG.className}`}
                     >
